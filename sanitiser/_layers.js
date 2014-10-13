6b29c5bbfe2b6372a92c61ca619d24b65c45be3c
Harish Krishna <harish.krsn@gmail.com>
2014-10-13 12:14:14 -0400

    adding 'aliases' - 'poi' & 'admin' for the layers param.

--- sanitiser/_layers.js
+++ sanitiser/_layers.js
@@ -13,17 +13,38 @@ function sanitize( req ){
 
   // which layers to query
   if('string' === typeof params.layers && params.layers.length){
+    
+    var alias_layers  = ['poi', 'admin'];
+    var alias_indeces = indeces.concat(alias_layers);
+
     var layers = params.layers.split(',').map( function( layer ){
       return layer.toLowerCase(); // lowercase inputs
     });
     for( var x=0; x<layers.length; x++ ){
-      if( -1 === indeces.indexOf( layers[x] ) ){
+      if( -1 === alias_indeces.indexOf( layers[x] ) ){
         return {
           'error': true,
-          'message': 'invalid param \'layer\': must be one or more of ' + indeces.join(',') 
+          'message': 'invalid param \'layer\': must be one or more of ' + alias_indeces.join(',') 
         }
       }
     }
+    var expand_aliases = function(alias, layers, layer_indeces) {
+      var alias_index  = layers.indexOf(alias);
+      if (alias_index !== -1 ) {
+        layers.splice(alias_index, 1);
+        layers = layers.concat(layer_indeces);
+      }
+      return layers;
+    }
+
+    layers = expand_aliases('poi',   layers, ['geoname','osmnode','osmway']);
+    layers = expand_aliases('admin', layers, ['admin0','admin1','admin2','neighborhood']);
+    
+    // de-dup
+    layers = layers.filter(function(item, pos) {
+      return layers.indexOf(item) == pos;
+    });
+
     clean.layers = layers;
   }
   else {
