32e9a13d64ce05d3066e6d7b7138c915f5e55e7a
Harish Krishna <harish.krsn@gmail.com>
2014-10-15 14:12:30 -0400

    type should be one of the indeces

--- sanitiser/_id.js
+++ sanitiser/_id.js
@@ -4,21 +4,22 @@
 
 function sanitize( req ){
   
-  req.clean = req.clean || {};
-  var params= req.query;
+  req.clean   = req.clean || {};
+  var params  = req.query;
+  var indeces = require('../query/indeces');
 
   // ensure params is a valid object
   if( Object.prototype.toString.call( params ) !== '[object Object]' ){
     params = {};
   }
 
-  var errormessage = function(fieldname) {
+  var errormessage = function(fieldname, message) {
     return {
       'error': true,
-      'message': 'invalid param \''+ fieldname + '\': text length, must be >0'
+      'message': message || ('invalid param \''+ fieldname + '\': text length, must be >0')
     }
   };
-  
+
   // id text
   if('string' !== typeof params.id || !params.id.length){
     return errormessage('id');
@@ -28,6 +29,12 @@ function sanitize( req ){
   // type text
   if('string' !== typeof params.type || !params.type.length){
     return errormessage('type');
+  }
+  req.clean.type = params.type;
+
+  // type text must be one of the indeces
+  if(indeces.indexOf(params.type) == -1){
+    return errormessage('type', 'type must be one of these values - [' + indeces.join(", ") + ']');
   }
   req.clean.type = params.type;
 
