fcd60463cca0cf61137d0e2e130f6a8cf72b923e
Peter Johnson <insomnia@rcpt.at>
2014-11-03 17:41:37 +0000

    remove geojsonify.suggest

--- test/unit/helper/geojsonify.js
+++ test/unit/helper/geojsonify.js
@@ -4,72 +4,12 @@ var geojsonify = require('../../../helper/geojsonify');
 module.exports.tests = {};
 
 module.exports.tests.interface = function(test, common) {
-  test('valid interface .suggest()', function(t) {
-    t.equal(typeof geojsonify.suggest, 'function', 'suggest is a function');
-    t.equal(geojsonify.suggest.length, 1, 'accepts x arguments');
+  test('valid interface .search()', function(t) {
+    t.equal(typeof geojsonify.search, 'function', 'search is a function');
+    t.equal(geojsonify.search.length, 1, 'accepts x arguments');
     t.end();
   });
 };
-
-module.exports.tests.suggest = function(test, common) {
-
-  var input = [{
-    bingo1: 'bango1',
-    payload: {
-      id: 'foo1/bar1',
-      geo: '100,-10.5'
-    }
-  },{
-    bingo2: 'bango2',
-    payload: {
-      id: 'foo2/bar2',
-      geo: '10,-1.5'
-    }
-  }];
-
-  var expected = {
-    "type": "FeatureCollection",
-    "features": [
-      {
-        "type": "Feature",
-        "geometry": {
-          "type": "Point",
-          "coordinates": [
-            100,
-            -10.5
-          ]
-        },
-        "properties": {
-          "bingo1": "bango1",
-          "type": "foo1",
-          "id": "bar1"
-        }
-      },
-      {
-        "type": "Feature",
-        "geometry": {
-          "type": "Point",
-          "coordinates": [
-            10,
-            -1.5
-          ]
-        },
-        "properties": {
-          "bingo2": "bango2",
-          "type": "foo2",
-          "id": "bar2"
-        }
-      }
-    ]
-  };
-
-  test('geojsonify.suggest()', function(t) {
-    var json = geojsonify.suggest( input );
-    t.deepEqual(json, expected, 'all docs mapped');
-    t.end();
-  });
-};
-
 
 module.exports.tests.search = function(test, common) {
 
