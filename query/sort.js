144b72cb162e2c5aac4e7c03d24f31163a32b793
Severyn Kozak <severyn.kozak@gmail.com>
2015-04-23 10:49:23 -0400

    Boost results with exact text matches when _score ties.
    query/sort.js
    	-Use the elasticsearch script introduced in
    	https://github.com/pelias/scripts/pull/7 to boost results with
    	exact text matches when documents' `_score`s are tied.
    	-Make `query/sort` export a function that creates the `sort`
    	query component, rather than the query component as a singleton,
    	to allow it to optionally add the exact-match script (that is,
    	when an `input` is present in `params`).

--- query/sort.js
+++ query/sort.js
@@ -3,36 +3,53 @@ var population = 'population';
 var popularity = 'popularity';
 var weights = require('pelias-suggester-pipeline').weights;
 
-module.exports = [
-  {
-    '_script': {
-      'file': admin_boost,
-      'type': 'number',
-      'order': 'desc'
+module.exports = function( params ){
+  var scriptsConfig = [
+    {
+      '_script': {
+        'file': admin_boost,
+        'type': 'number',
+        'order': 'desc'
+      }
+    },
+    {
+      '_script': {
+        'file': population,
+        'type': 'number',
+        'order': 'desc'
+      }
+    },
+    {
+      '_script': {
+        'file': popularity,
+        'type': 'number',
+        'order': 'desc'
+      }
+    },
+    {
+      '_script': {
+        'params': {
+          'weights': weights
+        },
+        'file': 'weights',
+        'type': 'number',
+        'order': 'desc'
+      }
     }
-  },
-  {
-    '_script': {
-      'file': population,
-      'type': 'number',
-      'order': 'desc'
-    }
-  },
-  {
-    '_script': {
-      'file': popularity,
-      'type': 'number',
-      'order': 'desc'
-    }
-  },
-  {
-    '_script': {
-      'params': {
-        'weights': weights
-      },
-      'file': 'weights',
-      'type': 'number',
-      'order': 'desc'
-    }
+  ];
+
+  if( typeof params === 'object' && params.hasOwnProperty( 'input' ) ){
+    scriptsConfig.push({
+      _script: {
+        params: {
+          input: params.input
+        },
+        file: 'exact_match',
+        type: 'number',
+        order: 'desc'
+      }
+    });
   }
-];
+
+  return scriptsConfig;
+};
