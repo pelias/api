d7c4409e2017542ed3b23bfe467aa44a81eadfdb
Severyn Kozak <severyn.kozak@gmail.com>
2015-04-23 10:54:51 -0400

    Fix failing tests.
    test/unit/query/(search, sort).js
    	-Fix the tests that started failing as a result of 144b72c.

--- test/unit/query/search.js
+++ test/unit/query/search.js
@@ -46,6 +46,16 @@ var sort = [
       'type': 'number',
       'order': 'desc'
     }
+  },
+  {
+    _script: {
+      params: {
+        input: 'test'
+      },
+      file: 'exact_match',
+      type: 'number',
+      order: 'desc'
+    }
   }
 ];
 
