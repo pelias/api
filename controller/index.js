29838e5b8c7c7ed7018878bcaaa535730d8dabe5
Severyn Kozak <severyn.kozak@gmail.com>
2015-04-24 12:22:51 -0400

    Move code that needs to run only once.
    controller/index.js
    	-Move all of the index page HTML "initialization" code that only
    	needs to run once, like markdown compilation and `style`
    	prepending, out of the request handler.

--- controller/index.js
+++ controller/index.js
@@ -5,16 +5,15 @@ var fs = require('fs');
 
 function setup(){
 
+  var styleString = '<style>html{font-family:monospace}</style>';
   var text = '# Pelias API\n';
   text += '### Version: ['+ pkg.version+ '](https://github.com/pelias/api/releases)\n';
   text += fs.readFileSync( './DOCS.md', 'utf8');
+  var indexHtml = styleString + markdown.toHTML(text);
 
   function controller( req, res, next ) {
     if (req.accepts('html')) {
-      if( text ) {
-        var style = '<style>html{font-family:monospace}</style>';
-        res.send(style + markdown.toHTML(text));
-      }
+      res.send(indexHtml);
       return;
     }
     // default behaviour
