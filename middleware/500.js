cf1a483edecf42ab4f2acaf8adf42996c7078e31
Severyn Kozak <severyn.kozak@gmail.com>
2015-04-27 12:05:48 -0400

    Don't report full errors in API results.
    middleware/500.js
    	-Return a generic error message rather than stringified error
    	object in API results.

--- middleware/500.js
+++ middleware/500.js
@@ -6,7 +6,7 @@ function middleware(err, req, res, next) {
   logger.error( 'Stack trace:', err.trace );
   res.header('Cache-Control','no-cache');
   if( res.statusCode < 400 ){ res.status(500); }
-  res.json({ error: err.toString() });
+  res.json({ error: 'internal server error' });
 }
 
 module.exports = middleware;
