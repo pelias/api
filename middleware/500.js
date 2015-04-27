17bb6db6e56e5c6b96ab1d9b06d57889d1b651c9
Severyn Kozak <severyn.kozak@gmail.com>
2015-04-27 11:53:29 -0400

    Resolve #114: report runtime errors.
    middleware/500.js
    	-Add code to the 500 catchall middleware to report errors to
    	stdout/stderr.
    	-Also, stringify the error via `.toString()` before passing it
    	to `res.json()`, since otherwise an empty object appears to be
    	returned.

--- middleware/500.js
+++ middleware/500.js
@@ -1,9 +1,12 @@
+var logger = require( '../src/logger' );
 
 // handle application errors
 function middleware(err, req, res, next) {
+  logger.error( 'Error:', err );
+  logger.error( 'Stack trace:', err.trace );
   res.header('Cache-Control','no-cache');
   if( res.statusCode < 400 ){ res.status(500); }
-  res.json({ error: err });
+  res.json({ error: err.toString() });
 }
 
 module.exports = middleware;
