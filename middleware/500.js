0b8220cbf396063f5554e742c59c22212e2c18c7
Severyn Kozak <severyn.kozak@gmail.com>
2015-05-05 14:40:53 -0400

    Log only once per error.
    middleware/500.js
    	-Make one log call per error rather than two, to reduce
    	clutter/noise.

--- middleware/500.js
+++ middleware/500.js
@@ -2,8 +2,7 @@ var logger = require( 'pelias-logger' ).get( 'middleware-500' );
 
 // handle application errors
 function middleware(err, req, res, next) {
-  logger.error( err );
-  logger.error( 'Stack trace:', err.trace );
+  logger.error( 'Error: `%s`. Stack trace: `%s`.', err, err.stack );
   res.header('Cache-Control','no-cache');
   if( res.statusCode < 400 ){ res.status(500); }
   res.json({ error: typeof err === 'string' ? err : 'internal server error' });
