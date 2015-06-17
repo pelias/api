b816e63ec055751f88a949b96f6b8008499dcae1
Diana Shkolnikov <dianashk@gmail.com>
2015-06-16 22:09:43 -0400

    more

--- ci/configure_vagrant.sh
+++ ci/configure_vagrant.sh
@@ -9,14 +9,19 @@ sudo dpkg -i chefdk_0.6.2-1_amd64.deb
 sudo apt-get install -q virtualbox --fix-missing
 wget https://dl.bintray.com/mitchellh/vagrant/vagrant_1.7.2_x86_64.deb
 sudo dpkg -i vagrant_1.7.2_x86_64.deb
+sudo vagrant plugin install vagrant-berkshelf
+sudo vagrant plugin install vagrant-omnibus
+
 git clone https://github.com/pelias/vagrant.git
 git clone https://github.com/pelias/acceptance-tests.git
 
-ls
-echo ${PWD}
-
-export PELIAS_VAGRANT_CFG=${PWD}/ci/pelias_settings.rb
 cd ./vagrant
+export PELIAS_VAGRANT_CFG=${PWD}/ci/pelias_settings.rb
+export PELIAS_VAGRANTFILE=${PWD}/ci/pelias_settings.rb
 vagrant up
+
 cd ../acceptance-tests
 node test -e local -t dev
+
+cd ../vagrant
+vagrant halt
