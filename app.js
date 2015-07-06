21d658531415712760fcd24a59e7f6a711f497ea
Peter Johnson <insomnia@rcpt.at>
2015-07-06 17:20:36 +0200

    update sanitizer middleware

--- app.js
+++ app.js
@@ -37,28 +37,17 @@ app.get( '/', controllers.index() );
 // doc API
 app.get( '/doc', sanitisers.doc.middleware, controllers.doc() );
 
-// suggest API
-// app.get( '/suggest', sanitisers.suggest.middleware, controllers.suggest() );
-// app.get( '/suggest/nearby', 
-// 		sanitisers.suggest.middleware, 
-// 		controllers.suggest(undefined, undefined, require('./helper/queryMixer').suggest_nearby) );
-// app.get( '/suggest/coarse', 
-// 		sanitisers.coarse.middleware, 
-// 		controllers.suggest(undefined, undefined, require('./helper/queryMixer').coarse) );
-
-// search API
-app.get( '/search', sanitisers.search.middleware, controllers.search() );
+// suggest APIs
 app.get( '/suggest', sanitisers.search.middleware, controllers.search() );
-app.get( '/suggest/nearby', sanitisers.search.middleware, controllers.search() );
-// @todo: /suggest/nearby should enforce lat/lon required, check other endpoints for validation
+app.get( '/suggest/nearby', sanitisers.suggest.middleware, controllers.search() );
+app.get( '/suggest/coarse', sanitisers.coarse.middleware, controllers.search() );
+
+// search APIs
+app.get( '/search', sanitisers.search.middleware, controllers.search() );
+app.get( '/search/coarse', sanitisers.coarse.middleware, controllers.search() );
 
 // reverse API
 app.get( '/reverse', sanitisers.reverse.middleware, controllers.search(undefined, require('./query/reverse')) );
-
-// coarse API
-app.get( '/search/coarse', sanitisers.coarse.middleware, controllers.search() );
-app.get( '/suggest/coarse', sanitisers.coarse.middleware, controllers.search() );
-
 
 /** ----------------------- error middleware ----------------------- **/
 
