f2197c4dd242660bab875dce1df1c3cdbdf6c06d
Harish Krishna <harish.krsn@gmail.com>
2015-07-14 14:01:59 -0400

    middleware fix

--- middleware/408.js
+++ middleware/408.js
@@ -2,8 +2,12 @@
 // handle time out errors
 function middleware(err, req, res, next) {
   res.header('Cache-Control','no-cache');
-  if( res.statusCode === 408 ){ res.status(408); }
-  res.json({ error: typeof err === 'string' ? err : 'request time out' });
+  if( res.statusCode === 408 ){ 
+  	res.status(408); 
+  	res.json({ error: err && typeof err.message === 'string' ? err.message : 'request time out' });
+  } else {
+  	next(err);
+  }
 }
 
 module.exports = middleware;
--- middleware/500.js
+++ middleware/500.js
@@ -5,7 +5,7 @@ function middleware(err, req, res, next) {
   logger.error( 'Error: `%s`. Stack trace: `%s`.', err, err.stack );
   res.header('Cache-Control','no-cache');
   if( res.statusCode < 400 ){ res.status(500); }
-  res.json({ error: typeof err === 'string' ? err : 'internal server error' });
+  res.json({ error: err && typeof err.message === 'string' ? err.message : 'internal server error' });
 }
 
 module.exports = middleware;
