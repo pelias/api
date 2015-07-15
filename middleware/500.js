ae4e75784fb699f2c2ab77e099f5ac90d2d04eef
Harish Krishna <harish.krsn@gmail.com>
2015-07-15 15:18:51 -0400

    big fix - err is of type string if there is an error within express app - its of type object (with .message being string) in case of errors messages from the ES cluster

--- middleware/500.js
+++ middleware/500.js
@@ -4,8 +4,9 @@ var logger = require( 'pelias-logger' ).get( 'middleware-500' );
 function middleware(err, req, res, next) {
   logger.error( 'Error: `%s`. Stack trace: `%s`.', err, err.stack );
   res.header('Cache-Control','no-cache');
+  var error = err.message ? err.message : err;
   if( res.statusCode < 400 ){ res.status(500); }
-  res.json({ error: err && typeof err.message === 'string' ? err.message : 'internal server error' });
+  res.json({ error: typeof error === 'string' ? error : 'internal server error' });
 }
 
 module.exports = middleware;
