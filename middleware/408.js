b3f85ebc17d94e564eefc1132c6c6b4bfa486487
Harish Krishna <harish.krsn@gmail.com>
2015-07-20 16:41:03 -0400

    err & err.message instead of err.message

--- middleware/408.js
+++ middleware/408.js
@@ -2,7 +2,7 @@
 // handle time out errors
 function middleware(err, req, res, next) {
   res.header('Cache-Control','no-cache');
-  var error = err.message ? err.message : err;
+  var error = (err && err.message) ? err.message : err;
   
   if( res.statusCode === 408 || (error.toLowerCase().indexOf('request timeout') !== -1) ){ 
   	res.status(408); 
