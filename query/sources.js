426189e87fac3c45f39e631dfa6cd3f1dc714d0d
Julian Simioni <julian@simioni.org>
2015-09-04 12:09:30 -0400

    Add sources -> types mapping file

---
+++ query/sources.js
@@ -0,0 +1,10 @@
+/*
+ * Mapping from data sources to type values
+ */
+
+module.exports = {
+  'geonames': ['geoname'],
+  'openaddresses' : ['openaddresses'],
+  'quattroshapes': ['admin0', 'admin1', 'admin2', 'neighborhood', 'locality', 'local_admin'],
+  'openstreetmap' : ['osmaddress', 'osmnode', 'osmway']
+};
