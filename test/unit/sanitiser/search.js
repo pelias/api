5d69c8016b1b728e44ecd29b62a7d95e711b3070
Julian Simioni <julian@simioni.org>
2015-09-04 18:44:45 -0400

    Remove lat/lon params from tests that don't use it
    This just removes clutter and possible confusion from our test cases.

--- test/unit/sanitiser/search.js
+++ test/unit/sanitiser/search.js
@@ -249,7 +249,7 @@ module.exports.tests.sanitize_details = function(test, common) {
   var valid_values = ['true', true, 1, '1', 'yes', 'y'];
   valid_values.forEach(function(details) {
     test('valid details param ' + details, function(t) {
-      sanitize({ text: 'test', lat: 0, lon: 0, details: details }, function( err, clean ){
+      sanitize({ text: 'test', details: details }, function( err, clean ){
         t.equal(clean.details, true, 'details set to true');
         t.end();
       });
@@ -259,7 +259,7 @@ module.exports.tests.sanitize_details = function(test, common) {
   var valid_false_values = ['false', false, 0, '0', 'no', 'n'];
   valid_false_values.forEach(function(details) {
     test('test setting false explicitly ' + details, function(t) {
-      sanitize({ text: 'test', lat: 0, lon: 0, details: details }, function( err, clean ){
+      sanitize({ text: 'test', details: details }, function( err, clean ){
         t.equal(clean.details, false, 'details set to false');
         t.end();
       });
@@ -267,7 +267,7 @@ module.exports.tests.sanitize_details = function(test, common) {
   });
 
   test('test default behavior', function(t) {
-    sanitize({ text: 'test', lat: 0, lon: 0 }, function( err, clean ){
+    sanitize({ text: 'test' }, function( err, clean ){
       t.equal(clean.details, true, 'details set to true');
       t.end();
     });
