a2cc31f142cb0b197d3c08db9b0274b296097439
Julian Simioni <julian@simioni.org>
2015-09-22 14:12:12 -0400

    Leave a note for future us
    A small essay on Elasticsearch queries and fields and types.

--- controller/place.js
+++ controller/place.js
@@ -16,6 +16,15 @@ function setup( backend ){
     var query = req.clean.ids.map( function(id) {
       return {
         _index: 'pelias',
+        /*
+         * some gids aren't resolvable to a single type (ex: osmnode and osmway
+         * both have source osm and layer venue), so expect an array of
+         * possible values. It's important to use `type` here instead of
+         * `_type`, as the former actually queries against the type, and thus
+         * can accept multiple match values.  `_type`, on the other hand,
+         * simply changes the actual URL of the query sent to Elasticsearch to
+         * contain a type, which obviously can only take a single type.
+         */
         type: id.types,
         _id: id.id
       };
