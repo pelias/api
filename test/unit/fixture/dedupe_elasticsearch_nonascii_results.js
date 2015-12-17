df93be7543fc37d303d12fc93c3c988191744161
Diana Shkolnikov <dianashk@gmail.com>
2015-12-17 13:51:06 -0500

    Fix deduping for non-ascii strings

---
+++ test/unit/fixture/dedupe_elasticsearch_nonascii_results.js
@@ -0,0 +1,61 @@
+module.exports = [
+  {
+    'id': 'foobar',
+    'gid': 'osm:venue:foobar',
+    'layer': 'venue',
+    'source': 'osm',
+    'name': {
+      'default': '万里长城万里长城'
+    },
+    'country_a': 'CHN',
+    'country': 'China',
+    'region': 'Beijing',
+    'confidence': 0.733
+  },
+  {
+    'id': '185883777',
+    'gid': 'osm:venue:185883777',
+    'layer': 'venue',
+    'source': 'osm',
+    'name': {
+      'default': '万里长城'
+    },
+    'country_a': 'CHN',
+    'country': 'China',
+    'region': 'Beijing',
+    'confidence': 0.733
+  },
+  {
+    'id': '1877602615',
+    'gid': 'osm:venue:1877602615',
+    'layer': 'venue',
+    'source': 'osm',
+    'name': {
+      'default': '万里花'
+    },
+    'country_a': 'JPN',
+    'country': 'Japan',
+    'region': 'Tokyo',
+    'county': '豊島区',
+    'locality': 'Tokyo',
+    'neighbourhood': '２丁目',
+    'confidence': 0.646
+  },
+  {
+    'id': '231404818',
+    'gid': 'osm:venue:231404818',
+    'layer': 'venue',
+    'source': 'osm',
+    'name': {
+      'default': '万里加油站'
+    },
+    'address': {
+      'street': 'S308',
+      'postalcode': '312044'
+    },
+    'country_a': 'CHN',
+    'country': 'China',
+    'region': 'Zhejiang',
+    'confidence': 0.646
+  }
+];
