94496d82ab9551140fd1339518c59110437bd901
missinglink <insomnia@rcpt.at>
2016-02-24 12:37:54 +0100

    fixes for focus.point to work with wof integration, plus some weighting tweaks, see PR for more info

--- query/autocomplete_defaults.js
+++ query/autocomplete_defaults.js
@@ -30,10 +30,10 @@ module.exports = _.merge({}, peliasQuery.defaults, {
   'phrase:slop': 2,
 
   'focus:function': 'linear',
-  'focus:offset': '10km',
+  'focus:offset': '0km',
   'focus:scale': '250km',
   'focus:decay': 0.5,
-  'focus:weight': 3,
+  'focus:weight': 10,
 
   'function_score:score_mode': 'avg',
   'function_score:boost_mode': 'multiply',
@@ -90,6 +90,6 @@ module.exports = _.merge({}, peliasQuery.defaults, {
   'population:field': 'population',
   'population:modifier': 'log1p',
   'population:max_boost': 20,
-  'population:weight': 2
+  'population:weight': 3
 
 });
--- test/unit/fixture/autocomplete_linguistic_focus.js
+++ test/unit/fixture/autocomplete_linguistic_focus.js
@@ -35,35 +35,25 @@ module.exports = {
                       'lat': 29.49136,
                       'lon': -82.50622
                     },
-                    'offset': '10km',
+                    'offset': '0km',
                     'scale': '250km',
                     'decay': 0.5
                   }
                 },
-                'weight': 3
+                'weight': 10
               }],
               'score_mode': 'avg',
               'boost_mode': 'multiply',
               'filter': {
                 'or': [
                   {
-                    'type': {
-                      'value': 'osmnode'
+                    'term': {
+                      'layer': 'venue'
                     }
                   },
                   {
-                    'type': {
-                      'value': 'osmway'
-                    }
-                  },
-                  {
-                    'type': {
-                      'value': 'osmaddress'
-                    }
-                  },
-                  {
-                    'type': {
-                      'value': 'openaddresses'
+                    'term': {
+                      'layer': 'address'
                     }
                   }
                 ]
@@ -124,7 +114,7 @@ module.exports = {
                   'modifier': 'log1p',
                   'field': 'population'
                 },
-                'weight': 2
+                'weight': 3
               }]
             }
           }]
