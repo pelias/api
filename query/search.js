2df306d1250558878127fafe44d4eb1602e645d9
Vesa Meskanen <vesa.meskanen@logica.com>
2016-02-29 11:05:28 +0200

    Query view configuration works now and creates the original request

--- query/search.js
+++ query/search.js
@@ -18,24 +18,27 @@ if (query_settings && query_settings.views) { // external config for views
   views = require( './query_views.json' );
 }
 
+// Note: 'views' config values below are not sanitized. Api should stop if conf is bad.
 for(var type in views) { // type = score | filter
   var viewSet = views[type];
-  if(!query[type]) { // skip unknown
-    return;
-  }
-  for(var viewName in viewSet) {
-    var params = viewSet[viewName];
-    var param, option;
-    if (Array.isArray(params) ) {
-      if( typeof params[0] === 'string') {
-        param = params[0];
+  for(var i=0; i<viewSet.length; i++) {
+    var params = viewSet[i];
+
+    // parse parameters from an array with the following format:
+    //   [0]=view name, [1]=view's parameter type (string or func)
+    //   [2]=bool to select func in [1], [3]=optional scoring type
+    var viewName = params[0];
+    var param = null, option=null;
+    if(params.length >= 2) {
+      param = params[1];
+      if(params.length >= 3) {
+        if (param && params[2]) {  // param is name of a function parameter
+          param =  peliasQuery.view[param]; // name to function
+	}
+	if(params.length >= 4) {
+	  option = params[3]; // must | should ...
+	}
       }
-      else if (typeof params[1] === 'string') {
-        param =  peliasQuery.view[params[1]]; // func
-      }
-      option = params[2]; // must | should ...
-    } else { // default case: string param for view
-      param = params;
     }
     if(param) {
       query[type]( peliasQuery.view[viewName](param), option );
