7b5b9dee61b378ec1e99637ead4956e1eff02d3b
Stephen Hess <stephen.k.hess@gmail.com>
2016-03-01 17:04:43 -0500

    added comments illustrating the subtleties of label generation

--- helper/labelGenerator.js
+++ helper/labelGenerator.js
@@ -24,7 +24,28 @@ module.exports = function( record ){
   }
 
   // de-dupe, join, trim
+  // NOTE: while it may seem odd to call `uniq` on the list of label parts,
+  //  the effect is quite subtle.  Take, for instance, a result for "Lancaster, PA"
+  //  the pseudo-object is:
+  //  {
+  //    'name': 'Lancaster',
+  //    'locality': 'Lancaster',
+  //    'region_a': 'PA',
+  //    'country_a': 'USA'
+  //  }
+  //
+  //  the code up to this point generates the label:
+  //  `Lancaster, Lancaster, PA, USA`
+  //
+  //  then the `unique` call reduces this to:
+  //  `Lancaster, PA, USA`
+  //
+  //  this code doesn't have the same effect in the case of a venue or address
+  //  where the `name` field would contain the address or name of a point-of-interest
+  //
+  //  Also see https://github.com/pelias/api/issues/429 for other ways that this is bad
   return _.uniq( labelParts ).join(', ').trim();
+
 };
 
 function getSchema(country_a) {
