82e39fd81f103f21877bbb539750264813e402e3
Diana Shkolnikov <dianashk@gmail.com>
2016-04-07 14:49:22 -0400

    parent block properties should not be changed, only top level ones

--- test/unit/middleware/normalizeParentIds.js
+++ test/unit/middleware/normalizeParentIds.js
@@ -8,30 +8,33 @@ module.exports.tests.interface = function(test, common) {
     var input = {
       data: [{
         'parent': {
-          'country': ['United States'],
+          'country': ['United States'], // these shouldn't change
           'country_id': ['85633793'],
-          'country_a': ['USA'],
-          'macroregion': ['MacroRegion Name'],
-          'macroregion_id': ['foobar'],
-          'macroregion_a': ['MacroRegion Abbreviation'],
-          'region': ['New York'],
-          'region_id': ['85688543'],
-          'region_a': ['NY'],
-          'macrocounty': ['MacroCounty Name'],
-          'macrocounty_id': ['~~~~~'],
-          'macrocounty_a': ['MacroCounty Abbreviation'],
-          'county': ['Kings County'],
-          'county_id': ['102082361'],
-          'county_a': [null],
-          'localadmin': ['Brooklyn'],
-          'localadmin_id': ['404521211'],
-          'localadmin_a': [null],
-          'locality': ['Some Locality'],
-          'locality_id': ['85977539'],
-          'locality_a': [null],
-          'neighbourhood': [],
-          'neighbourhood_id': []
-        }
+          'country_a': ['USA']
+        },
+        'country': ['United States'],
+        'country_id': ['85633793'],
+        'country_a': ['USA'],
+        'macroregion': ['MacroRegion Name'],
+        'macroregion_id': ['foobar'],
+        'macroregion_a': ['MacroRegion Abbreviation'],
+        'region': ['New York'],
+        'region_id': ['85688543'],
+        'region_a': ['NY'],
+        'macrocounty': ['MacroCounty Name'],
+        'macrocounty_id': ['~~~~~'],
+        'macrocounty_a': ['MacroCounty Abbreviation'],
+        'county': ['Kings County'],
+        'county_id': ['102082361'],
+        'county_a': [null],
+        'localadmin': ['Brooklyn'],
+        'localadmin_id': ['404521211'],
+        'localadmin_a': [null],
+        'locality': ['Some Locality'],
+        'locality_id': ['85977539'],
+        'locality_a': [null],
+        'neighbourhood': [],
+        'neighbourhood_id': []
       }]
     };
 
@@ -39,29 +42,32 @@ module.exports.tests.interface = function(test, common) {
       data: [{
         'parent': {
           'country': ['United States'],
-          'country_id': ['whosonfirst:country:85633793'],
-          'country_a': ['USA'],
-          'macroregion': ['MacroRegion Name'],
-          'macroregion_id': ['whosonfirst:macroregion:foobar'],
-          'macroregion_a': ['MacroRegion Abbreviation'],
-          'region': ['New York'],
-          'region_id': ['whosonfirst:region:85688543'],
-          'region_a': ['NY'],
-          'macrocounty': ['MacroCounty Name'],
-          'macrocounty_id': ['whosonfirst:macrocounty:~~~~~'],
-          'macrocounty_a': ['MacroCounty Abbreviation'],
-          'county': ['Kings County'],
-          'county_id': ['whosonfirst:county:102082361'],
-          'county_a': [null],
-          'localadmin': ['Brooklyn'],
-          'localadmin_id': ['whosonfirst:localadmin:404521211'],
-          'localadmin_a': [null],
-          'locality': ['Some Locality'],
-          'locality_id': ['whosonfirst:locality:85977539'],
-          'locality_a': [null],
-          'neighbourhood': [],
-          'neighbourhood_id': []
-        }
+          'country_id': ['85633793'],
+          'country_a': ['USA']
+        },
+        'country': ['United States'],
+        'country_id': ['whosonfirst:country:85633793'],
+        'country_a': ['USA'],
+        'macroregion': ['MacroRegion Name'],
+        'macroregion_id': ['whosonfirst:macroregion:foobar'],
+        'macroregion_a': ['MacroRegion Abbreviation'],
+        'region': ['New York'],
+        'region_id': ['whosonfirst:region:85688543'],
+        'region_a': ['NY'],
+        'macrocounty': ['MacroCounty Name'],
+        'macrocounty_id': ['whosonfirst:macrocounty:~~~~~'],
+        'macrocounty_a': ['MacroCounty Abbreviation'],
+        'county': ['Kings County'],
+        'county_id': ['whosonfirst:county:102082361'],
+        'county_a': [null],
+        'localadmin': ['Brooklyn'],
+        'localadmin_id': ['whosonfirst:localadmin:404521211'],
+        'localadmin_a': [null],
+        'locality': ['Some Locality'],
+        'locality_id': ['whosonfirst:locality:85977539'],
+        'locality_a': [null],
+        'neighbourhood': [],
+        'neighbourhood_id': []
       }]
     };
 
