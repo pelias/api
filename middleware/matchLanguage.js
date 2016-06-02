a2fa1665bf770199bf47c81c49e03d20018cba82
Vesa Meskanen <vesa.meskanen@logica.com>
2016-06-02 13:13:13 +0300

    language selection priority by matching is now more obvious
    1. Good match 2. api parameter 3. peliasconfig api.languages list

--- middleware/matchLanguage.js
+++ middleware/matchLanguage.js
@@ -6,19 +6,28 @@
  *  (or not realized to change) the language parameter in the client app. Language
  *  parameter is just a faint hint of preferences - the actual search string
  *  truly shows what is wanted.
+ *
+ *  Language selection priority:
+ *
+ *  1. Best match
+ *  2. Api 'lang' parameter or 'default' if not given
+ *  3. Peliasconfig 'languages' list order
  */
 
 var fuzzy = require('fuzzy.js');
 var logger = require('pelias-logger').get('api');
-var languages;
+var languages = ['default'];
+var languageMatchThreshold = 0.7;
 
 function setup(peliasConfig) {
+  peliasConfig = peliasConfig || require('pelias-config').generate().api;
+
   if(peliasConfig) {
-    languages = peliasConfig.languages;
+    languages = peliasConfig.languages || languages;
+    languageMatchThreshold = peliasConfig.languageMatchThreshold || languageMatchThreshold;
   }
   return matchLanguage;
 }
-
 
 function removeNumbers(val) {
   return val.replace(/[0-9]/g, '').trim();
@@ -47,35 +56,44 @@ function matchLanguage(req, res, next) {
   name = removeNumbers(name);
 
   // match name versions of 1st search result with the searched name
-  var bestLang;
-  var bestScore=0;
-  var fullScore = fuzzy(name, name).score;
-  var names = res.data[0].name; // 1st hit from ES = best so far
+  var names = res.data[0].name; // use 1st hit from ES = best match so far
 
-  for(var lang in names) {
+  var bestLang;
+  var bestScore = -1;
+  var fullScore = fuzzy(name, name).score;
+
+  var matchLang = function(lang) { // compute matching score
     var name2 = removeNumbers(names[lang]);
-    var score = fuzzy(name, name2).score;
-    if (score > bestScore ) {
+    return fuzzy(name, name2).score;
+  };
+
+  var updateBest = function(lang, score) {
       bestScore = score;
       bestLang = lang;
-    } else if (score === bestScore) {
-      if (lang === currentLang || lang === 'default' || !bestLang) {
-	// favor defaults
-	bestScore = score;
-	bestLang = lang;
-      } else if (languages) {
-	//judge by configured priority
+  };
+
+  for(var lang in names) {
+    var score = matchLang(lang);
+    if (score > bestScore ) {
+      updateBest(lang, score);
+    }
+    else if (score === bestScore && bestLang !== currentLang) {
+      // explicit lang parameter has 2nd highest priority
+      if (lang === currentLang) {
+	updateBest(lang, score);
+      }
+      else {
+	// judge by configured language list priority
 	var i1 = languages.indexOf(lang);
 	var i2 = languages.indexOf(bestLang);
 	if (i1 !== -1 && (i2 === -1 ||  i1 < i2)) {
-	  bestScore = score;
-	  bestLang = lang;
+	  updateBest(lang, score);
 	}
       }
     }
   }
   // change lang if best hit is good enough
-  if (bestLang && bestScore/fullScore > 0.8) {
+  if (bestLang && bestScore/fullScore > languageMatchThreshold) {
     // logger.debug('Best match by lang ' + bestLang );
     req.clean.lang = bestLang;
   }
