f0ddf75d00ac110507779eb5f26f56f4cc86519c
Vesa Meskanen <vesa.meskanen@logica.com>
2016-06-13 13:56:02 +0300

    New test which checks that geocoding results come out sorted by confidence score.
    Also, fix another test which expected wrong sort order.

--- test/unit/middleware/confidenceScore.js
+++ test/unit/middleware/confidenceScore.js
@@ -80,7 +80,7 @@ module.exports.tests.confidenceScore = function(test, common) {
           county: ['city1']
         }
       }, {
-        _score: 20,
+        _score: 5,
         value: 2,
         center_point: { lat: 100.2, lon: -51.5 },
         name: { default: 'test name2' },
@@ -133,6 +133,30 @@ module.exports.tests.confidenceScore = function(test, common) {
     t.equal(res.data[0].confidence, 0.6, 'Internationalized name contributed in scoring');
     t.equal(res.data[1].confidence, 0.54, 'Do not consider name versions excluded from configuration');
 
+    t.end();
+  });
+
+  test('sort results by confidence score', function(t) {
+    var req = {
+      clean: { text: 'rank me first' }
+    };
+    var res = {
+      data: [{
+        _score: 10,
+        value: 1,
+        center_point: { lat: 100.1, lon: -50.5 },
+        name: { default: 'rank me second' },
+      }, {
+        _score: 10, // force equal score before conf.scoring
+        value: 2,
+        center_point: { lat: 100.2, lon: -51.5 },
+        name: { default: 'rank me first' }, // better match here
+      }],
+      meta: {scores: [10]}
+    };
+
+    confidenceScore(req, res, function() {});
+    t.equal(res.data[0].name.default, 'rank me first', 'results are sorted by confidence score');
     t.end();
   });
 
