b0aab1afd3393eba5d7faacc66ee75def0d25524
Diana Shkolnikov <dianashk@gmail.com>
2016-07-29 11:37:39 -0400

    Fixed broken test due to rebase with changes from master.

--- test/unit/fixture/search_with_category_filtering.js
+++ test/unit/fixture/search_with_category_filtering.js
@@ -1,93 +1,86 @@
-
 module.exports = {
   'query': {
-    'filtered': {
-      'query': {
-        'bool': {
-          'must': [{
-            'match': {
-              'name.default': {
-                'query': 'test',
-                'boost': 1,
-                'analyzer': 'peliasIndexOneEdgeGram'
-              }
-            }
-          }],
-          'should': [{
+    'bool': {
+      'must': [{
+        'match': {
+          'name.default': {
+            'query': 'test',
+            'boost': 1,
+            'analyzer': 'peliasQueryFullToken'
+          }
+        }
+      }],
+      'should': [{
+        'match': {
+          'phrase.default': {
+            'query': 'test',
+            'analyzer': 'peliasPhrase',
+            'type': 'phrase',
+            'boost': 1,
+            'slop': 2
+          }
+        }
+      }, {
+        'function_score': {
+          'query': {
             'match': {
               'phrase.default': {
                 'query': 'test',
                 'analyzer': 'peliasPhrase',
                 'type': 'phrase',
-                'boost': 1,
-                'slop': 2
+                'slop': 2,
+                'boost': 1
               }
             }
-          }, {
-            'function_score': {
-              'query': {
-                'match': {
-                  'phrase.default': {
-                    'query': 'test',
-                    'analyzer': 'peliasPhrase',
-                    'type': 'phrase',
-                    'slop': 2,
-                    'boost': 1
-                  }
-                }
-              },
-              'max_boost': 20,
-              'score_mode': 'first',
-              'boost_mode': 'replace',
-              'functions': [{
-                'field_value_factor': {
-                  'modifier': 'log1p',
-                  'field': 'popularity',
-                  'missing': 1
-                },
-                'weight': 1
-              }]
-            }
-          }, {
-            'function_score': {
-              'query': {
-                'match': {
-                  'phrase.default': {
-                    'query': 'test',
-                    'analyzer': 'peliasPhrase',
-                    'type': 'phrase',
-                    'slop': 2,
-                    'boost': 1
-                  }
-                }
-              },
-              'max_boost': 20,
-              'score_mode': 'first',
-              'boost_mode': 'replace',
-              'functions': [{
-                'field_value_factor': {
-                  'modifier': 'log1p',
-                  'field': 'population',
-                  'missing': 1
-                },
-                'weight': 2
-              }]
-            }
+          },
+          'max_boost': 20,
+          'score_mode': 'first',
+          'boost_mode': 'replace',
+          'functions': [{
+            'field_value_factor': {
+              'modifier': 'log1p',
+              'field': 'popularity',
+              'missing': 1
+            },
+            'weight': 1
           }]
         }
-      },
-      'filter': {
-        'bool': {
-          'must': [{
-            'terms': {
-              'category': ['retail','food']
+      }, {
+        'function_score': {
+          'query': {
+            'match': {
+              'phrase.default': {
+                'query': 'test',
+                'analyzer': 'peliasPhrase',
+                'type': 'phrase',
+                'slop': 2,
+                'boost': 1
+              }
             }
+          },
+          'max_boost': 20,
+          'score_mode': 'first',
+          'boost_mode': 'replace',
+          'functions': [{
+            'field_value_factor': {
+              'modifier': 'log1p',
+              'field': 'population',
+              'missing': 1
+            },
+            'weight': 2
           }]
         }
-      }
+      }],
+      'filter': [{
+        'terms': {
+          'category': ['retail', 'food']
+        }
+      }]
     }
   },
-  'sort': [ '_score' ],
   'size': 20,
-  'track_scores': true
+  'track_scores': true,
+  'sort': [
+    '_score'
+  ]
 };
