39e3156019c8969487a549f65b1108b793bd1c56
Peter Johnson <insomnia@rcpt.at>
2016-08-02 12:51:56 +0200

    typo

--- test/unit/sanitiser/_geo_common.js
+++ test/unit/sanitiser/_geo_common.js
@@ -55,7 +55,7 @@ module.exports.tests.wrapping = function(test, common) {
     };
     sanitize.sanitize_point( 'point', clean, params, false );
     t.equal(clean['point.lat'], +88.9, 'equal to (+90 - 1.1)');
-    t.equal(clean['point.lon'], -178.9, 'equal to (-180 - 1.1)'); // polar flip
+    t.equal(clean['point.lon'], -178.9, 'equal to (-180 + 1.1)'); // polar flip
     t.end();
   });
   test('negative latitudinal wrapping', function (t) {
