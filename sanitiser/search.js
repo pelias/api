7e119ff392bec579de517a9281e8bcd0cde1238c
Stephen Hess <stephen.k.hess@gmail.com>
2016-08-04 16:55:55 -0400

    refactored search sanitiser tests
    the tests removed were testing far more than was appropriate since the functionality of the individual sanitisers is tested elsewhere.  in this case, the test has been reduced to just testing that all the sanitisers were actually called.

--- sanitiser/search.js
+++ sanitiser/search.js
@@ -8,7 +8,7 @@ var sanitizeAll = require('../sanitiser/sanitizeAll'),
       size: require('../sanitiser/_size')(/* use defaults*/),
       layers: require('../sanitiser/_targets')('layers', type_mapping.layer_mapping),
       sources: require('../sanitiser/_targets')('sources', type_mapping.source_mapping),
-      // depends on the layers and sources sanitisers, must be run after them
+      // // depends on the layers and sources sanitisers, must be run after them
       sources_and_layers: require('../sanitiser/_sources_and_layers'),
       private: require('../sanitiser/_flag_bool')('private', false),
       geo_search: require('../sanitiser/_geo_search'),
@@ -17,10 +17,6 @@ var sanitizeAll = require('../sanitiser/sanitizeAll'),
     };
 
 var sanitize = function(req, cb) { sanitizeAll(req, sanitizers, cb); };
-
-// export sanitize for testing
-module.exports.sanitize = sanitize;
-module.exports.sanitiser_list = sanitizers;
 
 // middleware
 module.exports.middleware = function( req, res, next ){
