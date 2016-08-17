ad5d2bf36a02d1dab445e438318be99b5f65a384
Stephen Hess <stephen.k.hess@gmail.com>
2016-08-17 13:55:56 -0400

    add early exit condition to stop unneeded re-parsing
    This is required in the case where the libpostal parse + ES search was successful (res.data is non-empty) so we want to skip falling back to the current production behavior.

--- sanitiser/search_fallback.js
+++ sanitiser/search_fallback.js
@@ -7,7 +7,15 @@ var sanitize = function(req, cb) { sanitizeAll(req, sanitizers, cb); };
 
 // middleware
 module.exports.middleware = function( req, res, next ){
+  // if res.data already has results then don't call the _text_autocomplete sanitiser
+  // this has been put into place for when the libpostal integration way of querying
+  // ES doesn't return anything and we want to fallback to the old logic
+  if (res && res.hasOwnProperty('data') && res.data.length > 0) {
+    return next();
+  }
+
   sanitize( req, function( err, clean ){
     next();
   });
+
 };
