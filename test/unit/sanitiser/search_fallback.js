455f31eb39dfdaa02927fe47563767343e3b615b
Stephen Hess <stephen.k.hess@gmail.com>
2016-08-17 13:55:56 -0400

    add early exit condition to stop unneeded re-parsing
    This is required in the case where the libpostal parse + ES search was successful (res.data is non-empty) so we want to skip falling back to the current production behavior.

--- test/unit/sanitiser/search_fallback.js
+++ test/unit/sanitiser/search_fallback.js
@@ -3,7 +3,32 @@ var proxyquire =  require('proxyquire').noCallThru();
 module.exports.tests = {};
 
 module.exports.tests.sanitize = function(test, common) {
-  test('verify that all sanitisers were called as expected', function(t) {
+  test('verify that all sanitisers were called as expected when `res` is undefined', function(t) {
+    var called_sanitisers = [];
+
+    // rather than re-verify the functionality of all the sanitisers, this test just verifies that they
+    //  were all called correctly
+    var search = proxyquire('../../../sanitiser/search_fallback', {
+      '../sanitiser/_text_autocomplete': function() {
+        called_sanitisers.push('_text_autocomplete');
+        return { errors: [], warnings: [] };
+      }
+    });
+
+    var expected_sanitisers = [
+      '_text_autocomplete'
+    ];
+
+    var req = {};
+
+    search.middleware(req, undefined, function(){
+      t.deepEquals(called_sanitisers, expected_sanitisers);
+      t.end();
+    });
+
+  });
+
+  test('verify that all sanitisers were called as expected when `res` has no `data` property', function(t) {
     var called_sanitisers = [];
 
     // rather than re-verify the functionality of all the sanitisers, this test just verifies that they
@@ -26,7 +51,62 @@ module.exports.tests.sanitize = function(test, common) {
       t.deepEquals(called_sanitisers, expected_sanitisers);
       t.end();
     });
+
   });
+
+  test('verify that all sanitisers were called as expected when res.data is empty', function(t) {
+    var called_sanitisers = [];
+
+    // rather than re-verify the functionality of all the sanitisers, this test just verifies that they
+    //  were all called correctly
+    var search = proxyquire('../../../sanitiser/search_fallback', {
+      '../sanitiser/_text_autocomplete': function() {
+        called_sanitisers.push('_text_autocomplete');
+        return { errors: [], warnings: [] };
+      }
+    });
+
+    var expected_sanitisers = [
+      '_text_autocomplete'
+    ];
+
+    var req = {};
+    var res = {
+      data: []
+    };
+
+    search.middleware(req, res, function(){
+      t.deepEquals(called_sanitisers, expected_sanitisers);
+      t.end();
+    });
+
+  });
+
+  test('non-empty res.data should not call the _text_autocomplete sanitiser', function(t) {
+    var called_sanitisers = [];
+
+    // rather than re-verify the functionality of all the sanitisers, this test just verifies that they
+    //  were all called correctly
+    var search = proxyquire('../../../sanitiser/search_fallback', {
+      '../sanitiser/_text_autocomplete': function() {
+        throw new Error('_text_autocomplete sanitiser should not have been called');
+      }
+    });
+
+    var expected_sanitisers = [];
+
+    var req = {};
+    var res = {
+      data: [{}]
+    };
+
+    search.middleware(req, res, function(){
+      t.deepEquals(called_sanitisers, expected_sanitisers);
+      t.end();
+    });
+
+  });
+
 };
 
 module.exports.all = function (tape, common) {
