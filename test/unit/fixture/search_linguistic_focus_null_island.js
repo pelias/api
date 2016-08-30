badfa45417f2ffebf15d05f29b370e40d4466642
Stephen Hess <stephen.k.hess@gmail.com>
2016-08-30 14:01:34 -0400

    enabled all scores and filters
    adjusted tests to account for new querying strategies, renamed all existing test fixture files to `*_original`.  pointed query module to a PR for testing.

--- test/unit/fixture/search_linguistic_focus_null_island.js
+++ test/unit/fixture/search_linguistic_focus_null_island.js
@@ -1,117 +1,65 @@
-
 module.exports = {
   'query': {
-    'bool': {
-      'must': [{
-        'match': {
-          'name.default': {
-            'query': 'test',
-            'boost': 1,
-            'analyzer': 'peliasQueryFullToken'
+    'function_score': {
+      'query': {
+        'filtered': {
+          'query': {
+            'bool': {
+              'should': []
+            }
+          },
+          'filter': {
+            'bool': {
+              'must': [
+                {
+                  'terms': {
+                    'layer': [
+                      'test'
+                    ]
+                  }
+                }
+              ]
+            }
           }
         }
-      }],
-      'should': [{
-        'match': {
-          'phrase.default': {
-            'query': 'test',
-            'analyzer': 'peliasPhrase',
-            'type': 'phrase',
-            'boost': 1,
-            'slop': 2
+      },
+      'max_boost': 20,
+      'functions': [
+        {
+          'weight': 2,
+          'linear': {
+            'center_point': {
+              'origin': {
+                'lat': 0,
+                'lon': 0
+              },
+              'offset': '0km',
+              'scale': '50km',
+              'decay': 0.5
+            }
           }
-        }
-      }, {
-        'function_score': {
-          'query': {
-            'match': {
-              'phrase.default': {
-                'analyzer': 'peliasPhrase',
-                'type': 'phrase',
-                'boost': 1,
-                'slop': 2,
-                'query': 'test'
-              }
-            }
+        },
+        {
+          'field_value_factor': {
+            'modifier': 'log1p',
+            'field': 'popularity',
+            'missing': 1
           },
-          'functions': [{
-            'linear': {
-              'center_point': {
-                'origin': {
-                  'lat': 0,
-                  'lon': 0
-                },
-                'offset': '0km',
-                'scale': '50km',
-                'decay': 0.5
-              }
-            },
-            'weight': 2
-          }],
-          'score_mode': 'avg',
-          'boost_mode': 'replace'
-        }
-      },{
-        'function_score': {
-          'query': {
-            'match': {
-              'phrase.default': {
-                'query': 'test',
-                'analyzer': 'peliasPhrase',
-                'type': 'phrase',
-                'slop': 2,
-                'boost': 1
-              }
-            }
+          'weight': 1
+        },
+        {
+          'field_value_factor': {
+            'modifier': 'log1p',
+            'field': 'population',
+            'missing': 1
           },
-          'max_boost': 20,
-          'score_mode': 'first',
-          'boost_mode': 'replace',
-          'functions': [{
-            'field_value_factor': {
-              'modifier': 'log1p',
-              'field': 'popularity',
-              'missing': 1
-            },
-            'weight': 1
-          }]
+          'weight': 2
         }
-      },{
-        'function_score': {
-          'query': {
-            'match': {
-              'phrase.default': {
-                'query': 'test',
-                'analyzer': 'peliasPhrase',
-                'type': 'phrase',
-                'slop': 2,
-                'boost': 1
-              }
-            }
-          },
-          'max_boost': 20,
-          'score_mode': 'first',
-          'boost_mode': 'replace',
-          'functions': [{
-            'field_value_factor': {
-              'modifier': 'log1p',
-              'field': 'population',
-              'missing': 1
-            },
-            'weight': 2
-          }]
-        }
-      }],
-      'filter':[{
-        'terms': {
-          'layer': [
-            'test'
-          ]
-        }
-      }]
+      ],
+      'score_mode': 'avg',
+      'boost_mode': 'replace'
     }
   },
-  'sort': [ '_score' ],
   'size': 10,
   'track_scores': true
 };
--- test/unit/fixture/search_linguistic_viewport_min_diagonal.js
+++ test/unit/fixture/search_linguistic_viewport_min_diagonal.js
@@ -1,128 +1,51 @@
 module.exports = {
   'query': {
-    'filtered': {
+    'function_score': {
       'query': {
-        'bool': {
-          'must': [
-            {
-              'match': {
-                'name.default': {
-                  'analyzer': 'peliasQueryFullToken',
-                  'boost': 1,
-                  'query': 'test'
-                }
-              }
+        'filtered': {
+          'query': {
+            'bool': {
+              'should': []
             }
-          ],
-          'should': [
-            {
-              'match': {
-                'phrase.default': {
-                  'analyzer': 'peliasPhrase',
-                  'type': 'phrase',
-                  'boost': 1,
-                  'slop': 2,
-                  'query': 'test'
+          },
+          'filter': {
+            'bool': {
+              'must': [
+                {
+                  'terms': {
+                    'layer': [
+                      'test'
+                    ]
+                  }
                 }
-              }
-            },
-            {
-              'function_score': {
-                'query': {
-                  'match': {
-                    'phrase.default': {
-                      'analyzer': 'peliasPhrase',
-                      'type': 'phrase',
-                      'boost': 1,
-                      'slop': 2,
-                      'query': 'test'
-                    }
-                  }
-                },
-                'functions': [
-                  {
-                    'weight': 2,
-                    'linear': {
-                      'center_point': {
-                        'origin': {
-                          'lat': 28.49136,
-                          'lon': -87.50623
-                        },
-                        'offset': '0km',
-                        'scale': '1km',
-                        'decay': 0.5
-                      }
-                    }
-                  }
-                ],
-                'score_mode': 'avg',
-                'boost_mode': 'replace'
-              }
-            },
-            {
-              'function_score': {
-                'query': {
-                  'match': {
-                    'phrase.default': {
-                      'analyzer': 'peliasPhrase',
-                      'type': 'phrase',
-                      'boost': 1,
-                      'slop': 2,
-                      'query': 'test'
-                    }
-                  }
-                },
-                'max_boost': 20,
-                'functions': [
-                  {
-                    'field_value_factor': {
-                      'modifier': 'log1p',
-                      'field': 'popularity',
-                      'missing': 1
-                    },
-                    'weight': 1
-                  }
-                ],
-                'score_mode': 'first',
-                'boost_mode': 'replace'
-              }
-            },
-            {
-              'function_score': {
-                'query': {
-                  'match': {
-                    'phrase.default': {
-                      'analyzer': 'peliasPhrase',
-                      'type': 'phrase',
-                      'boost': 1,
-                      'slop': 2,
-                      'query': 'test'
-                    }
-                  }
-                },
-                'max_boost': 20,
-                'functions': [
-                  {
-                    'field_value_factor': {
-                      'modifier': 'log1p',
-                      'field': 'population',
-                      'missing': 1
-                    },
-                    'weight': 2
-                  }
-                ],
-                'score_mode': 'first',
-                'boost_mode': 'replace'
-              }
+              ]
             }
-          ]
+          }
         }
-      }
+      },
+      'max_boost': 20,
+      'functions': [
+        {
+          'field_value_factor': {
+            'modifier': 'log1p',
+            'field': 'popularity',
+            'missing': 1
+          },
+          'weight': 1
+        },
+        {
+          'field_value_factor': {
+            'modifier': 'log1p',
+            'field': 'population',
+            'missing': 1
+          },
+          'weight': 2
+        }
+      ],
+      'score_mode': 'avg',
+      'boost_mode': 'replace'
     }
   },
   'size': 10,
-  'track_scores': true,
-  'sort': [
-    '_score'
-  ]
+  'track_scores': true
 };
