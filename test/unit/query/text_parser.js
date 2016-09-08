adb15ed61860f76f0cadee53e7cb580616015c95
Stephen Hess <stephen.k.hess@gmail.com>
2016-09-08 12:23:43 -0400

    uncommented tests

--- test/unit/query/text_parser.js
+++ test/unit/query/text_parser.js
@@ -10,62 +10,62 @@ module.exports.tests.interface = function(test, common) {
   });
 };
 
-// module.exports.tests.query = function(test, common) {
-//   test('parsed_text without properties should leave vs properties unset', function(t) {
-//     var parsed_text = {};
-//     var vs = new VariableStore();
-//
-//     text_parser(parsed_text, vs);
-//
-//     t.false(vs.isset('input:query'));
-//     t.false(vs.isset('input:category'));
-//     t.false(vs.isset('input:housenumber'));
-//     t.false(vs.isset('input:street'));
-//     t.false(vs.isset('input:neighbourhood'));
-//     t.false(vs.isset('input:borough'));
-//     t.false(vs.isset('input:postcode'));
-//     t.false(vs.isset('input:locality'));
-//     t.false(vs.isset('input:county'));
-//     t.false(vs.isset('input:region'));
-//     t.false(vs.isset('input:country'));
-//     t.end();
-//
-//   });
-//
-//   test('parsed_text without properties should leave vs properties unset', function(t) {
-//     var parsed_text = {
-//       query: 'query value',
-//       category: 'category value',
-//       number: 'number value',
-//       street: 'street value',
-//       neighbourhood: 'neighbourhood value',
-//       borough: 'borough value',
-//       postalcode: 'postalcode value',
-//       city: 'city value',
-//       county: 'county value',
-//       state: 'state value',
-//       country: 'country value'
-//     };
-//     var vs = new VariableStore();
-//
-//     text_parser(parsed_text, vs);
-//
-//     t.equals(vs.var('input:query').toString(), 'query value');
-//     t.equals(vs.var('input:category').toString(), 'category value');
-//     t.equals(vs.var('input:housenumber').toString(), 'number value');
-//     t.equals(vs.var('input:street').toString(), 'street value');
-//     t.equals(vs.var('input:neighbourhood').toString(), 'neighbourhood value');
-//     t.equals(vs.var('input:borough').toString(), 'borough value');
-//     t.equals(vs.var('input:postcode').toString(), 'postalcode value');
-//     t.equals(vs.var('input:locality').toString(), 'city value');
-//     t.equals(vs.var('input:county').toString(), 'county value');
-//     t.equals(vs.var('input:region').toString(), 'state value');
-//     t.equals(vs.var('input:country').toString(), 'country value');
-//     t.end();
-//
-//   });
-//
-// };
+module.exports.tests.query = function(test, common) {
+  test('parsed_text without properties should leave vs properties unset', function(t) {
+    var parsed_text = {};
+    var vs = new VariableStore();
+
+    text_parser(parsed_text, vs);
+
+    t.false(vs.isset('input:query'));
+    t.false(vs.isset('input:category'));
+    t.false(vs.isset('input:housenumber'));
+    t.false(vs.isset('input:street'));
+    t.false(vs.isset('input:neighbourhood'));
+    t.false(vs.isset('input:borough'));
+    t.false(vs.isset('input:postcode'));
+    t.false(vs.isset('input:locality'));
+    t.false(vs.isset('input:county'));
+    t.false(vs.isset('input:region'));
+    t.false(vs.isset('input:country'));
+    t.end();
+
+  });
+
+  test('parsed_text without properties should leave vs properties unset', function(t) {
+    var parsed_text = {
+      query: 'query value',
+      category: 'category value',
+      number: 'number value',
+      street: 'street value',
+      neighbourhood: 'neighbourhood value',
+      borough: 'borough value',
+      postalcode: 'postalcode value',
+      city: 'city value',
+      county: 'county value',
+      state: 'state value',
+      country: 'country value'
+    };
+    var vs = new VariableStore();
+
+    text_parser(parsed_text, vs);
+
+    t.equals(vs.var('input:query').toString(), 'query value');
+    t.equals(vs.var('input:category').toString(), 'category value');
+    t.equals(vs.var('input:housenumber').toString(), 'number value');
+    t.equals(vs.var('input:street').toString(), 'street value');
+    t.equals(vs.var('input:neighbourhood').toString(), 'neighbourhood value');
+    t.equals(vs.var('input:borough').toString(), 'borough value');
+    t.equals(vs.var('input:postcode').toString(), 'postalcode value');
+    t.equals(vs.var('input:locality').toString(), 'city value');
+    t.equals(vs.var('input:county').toString(), 'county value');
+    t.equals(vs.var('input:region').toString(), 'state value');
+    t.equals(vs.var('input:country').toString(), 'country value');
+    t.end();
+
+  });
+
+};
 
 module.exports.tests.housenumber_special_cases = function(test, common) {
   test('numeric query with street but no number should reassign query to housenumber', function(t) {
