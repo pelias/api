0ff8af7c8e02d4f2def5a5c83a4ea7c3fb5a8059
Vesa Meskanen <vesa.meskanen@logica.com>
2016-09-15 12:23:43 +0300

    Address order should not be configurable
    Each country has a fixed way for ordering street and house number.
    So, new per country order specs should be added to source code,
    not to the config file.

--- helper/flipNumberAndStreetCountries.js
+++ helper/flipNumberAndStreetCountries.js
@@ -1,14 +1,5 @@
 var _ = require('lodash');
 
-var flipNumberAndStreetCountries = ['DEU', 'FIN', 'SWE', 'NOR', 'DNK', 'ISL', 'CZE'];
-
-var api = require('pelias-config').generate().api;
-var localization = api.localization;
-if (localization) {
-  if (localization.flipNumberAndStreetCountries) {
-    var countries = localization.flipNumberAndStreetCountries;
-    flipNumberAndStreetCountries = _.uniq(flipNumberAndStreetCountries.concat(countries));
-  }
-}
+var flipNumberAndStreetCountries = ['DEU', 'FIN', 'SWE', 'NOR', 'DNK', 'ISL', 'CZE', 'NLD'];
 
 module.exports = flipNumberAndStreetCountries;
