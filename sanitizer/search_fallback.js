a62581ddd6543329ca96ebcc55c9d2cf11dfeeed
Stephen Hess <stephen.k.hess@gmail.com>
2016-10-05 14:31:21 -0400

    actually removed last of `sanitise` references

--- sanitizer/search_fallback.js
+++ sanitizer/search_fallback.js
@@ -9,7 +9,7 @@ var logging = require( '../helper/logging' );
 
 // middleware
 module.exports.middleware = function( req, res, next ){
-  // if res.data already has results then don't call the _text_autocomplete sanitiser
+  // if res.data already has results then don't call the _text_autocomplete sanitizer
   // this has been put into place for when the libpostal integration way of querying
   // ES doesn't return anything and we want to fallback to the old logic
   if (res && res.hasOwnProperty('data') && res.data.length > 0) {
