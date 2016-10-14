5c73f4d7a9f2e15f05c521c86b504bc671f4f930
Vesa Meskanen <vesa.meskanen@logica.com>
2016-10-14 09:17:15 +0300

    Final cleanup & refactoring of confidence scoring:
    - parsed_text.regions now contains only valid data, no need to
    drop the first item in admin scoring
    - remove the rest of unused code
    - add new and fix incorrect comments
    - suitable level of debug logging about confidence scoring

--- middleware/confidenceScoreDT.js
+++ middleware/confidenceScoreDT.js
@@ -1,7 +1,4 @@
-
-
 /**
- *
  * Basic confidence score should be computed and returned for each item in the results.
  * The score should range between 0-1, and take into consideration as many factors as possible.
  */
@@ -106,6 +103,7 @@ function compareResults(a, b) {
   return 0;
 }
 
+
 function computeScores(req, res, next) {
   // do nothing if no result data set
   if (!check.assigned(req.clean) || !check.assigned(res) ||
@@ -118,6 +116,7 @@ function computeScores(req, res, next) {
 
   res.data.sort(compareResults);
 
+  // don't return poor results
   var bestConfidence = res.data[0].confidence;
   var limit = minConfidence;
   if(relativeMinConfidence) {
@@ -130,23 +129,10 @@ function computeScores(req, res, next) {
   next();
 }
 
-// simple helper to get two name variants from addresss without country context
-function namesFromAddress(street, number) {
-  var name1 = street || '';
-  var name2 = name1;
-  var space = (name1.length===0?'':' ');
-
-  if(check.assigned(number)) {
-    name1 = name1 + space  + number;
-    name2 = number + space + name2;
-  }
-  return [name1, name2];
-}
 
 /**
  * Check all types of things to determine how confident we are that this result
- * is correct. Score is based on overall score distribution in the result set
- * as well as how closely the result matches the text parameters.
+ * is correct.
  *
  * @param {object} req
  * @param {object} hit
@@ -154,50 +140,61 @@ function namesFromAddress(street, number) {
  */
 function computeConfidenceScore(req, hit) {
 
-  hit.confidence = 0;
-  var checkCount = 0;
   var parsedText = req.clean.parsed_text;
 
-  // first compare address parts one by one
+  // compare parsed name (or raw text) against configured language versions of name
+  hit.confidence = checkName(req.clean.text, parsedText, hit);
+  var weightSum=1;
+
+  // compare address parts one by one
   if (parsedText) {
     for(var key in confidenceAddressParts) {
       if(check.assigned(parsedText[key])) {
         hit.confidence += confidenceAddressParts[key].weight*checkAddressPart(parsedText, hit, key);
-        checkCount += confidenceAddressParts[key].weight;
+        weightSum += confidenceAddressParts[key].weight;
       }
     }
   }
 
-  // compare parsed name (or raw text) against configured language versions of name
-  hit.confidence += checkName(req.clean.text, parsedText, hit);
-  checkCount++;
-
+  // score admin areas such as city or neigbourhood
   if(adminWeights) {
-    var adminConf;
-    if(parsedText && parsedText.regions && parsedText.regions.length>1) {
-      // keep admin scoring proportion constant 50%
-      // regardless of count of finer scores
-      // so, score is max 0.5 if city is all wrong
-      hit.confidence += checkCount*checkRegions(parsedText, hit);
-      checkCount*=2;
+    var adminConfidence;
+    var weight;
+
+    if(parsedText && parsedText.regions) {
+
+      adminConfidence = checkAdmin(parsedText.regions, hit);
+
+      // Keep admin scoring proportion constant 50% regardless of the
+      // count of finer score factors. Score is max 0.5 if city is all wrong
+      weight = weightSum;
+
     } else if(hit.confidence<1) {
+
       // Text could not be parsed, and does not match any document perfectly.
       // There is a chance that text contains admin info like small city without
       // comma separation (libpostal misses those), or name is formatted loosely
       // 'tampereen keskustori'. So check raw text against admin areas
-      var weight = 1 - hit.confidence; // leftover from name match
-      hit.confidence += weight*checkAdmin(req.clean.text, hit);
-      checkCount+=weight;
+      adminConfidence = checkAdmin(req.clean.text, hit);
+      weight = 1 - hit.confidence; // leftover from name match
+    }
+
+    if(weight) {
+      logger.debug('admin confidence', adminConfidence);
+
+      hit.confidence += weight*adminConfidence;
+      weightSum+=weight;
     }
   }
 
   // TODO: look at categories
 
-  hit.confidence /= checkCount;
+  hit.confidence /= weightSum;
   logger.debug('### confidence', hit.confidence);
 
   return hit;
 }
+
 
 /**
  * Compare text string against configuration defined language versions of a property
@@ -228,10 +225,11 @@ function checkLanguageProperty(text, propertyObject, stripNumbers) {
       bestName = propertyObject[lang];
     }
   }
-  logger.debug('name score', bestScore, text, bestName);
+  logger.debug('name confidence', bestScore, text, bestName);
 
   return bestScore;
 }
+
 
 /**
  * Compare text string or name component of parsed_text against
@@ -254,22 +252,6 @@ function checkName(text, parsed_text, hit) {
   return(checkLanguageProperty(text, hit.name));
 }
 
-/**
- * text.number being set indicates the query was for an address
- * check if house number was specified and found in result
- *
- * @param {object|undefined} text
- * @param {object} hit
- * @returns {number}
- */
-function checkQueryType(text, hit) {
-  if (check.assigned(text) && check.assigned(text.number) &&
-      (!check.assigned(hit.address_parts) ||
-      (check.assigned(hit.address_parts) && !check.assigned(hit.address_parts.number)))) {
-    return 0;
-  }
-  return 1;
-}
 
 /**
  * Determine the quality of the property match
@@ -286,13 +268,12 @@ function propMatch(textProp, hitProp, numeric) {
     return 0.1;
   }
 
-  // both present
-  if (numeric) {
+  if (numeric) { // special treatment for numbers such as house number
     if(textProp === hitProp) {
       // handle exact match before dropping all but numeric part
       return 1.0;
     }
-    var n1 = parseInt(textProp);
+    var n1 = parseInt(textProp); // e.g. 4b -> 4, 3-5 -> 3
     var n2 = parseInt(hitProp);
     if (!isNaN(n1) && !isNaN(n2)) {
       return Math.sqrt(0.9/(1.0 + Math.abs(n1-n2)));
@@ -305,14 +286,13 @@ function propMatch(textProp, hitProp, numeric) {
 // array wrapper for function above
 function propMatchArray(text, hitProp, numeric) {
   if (Array.isArray(hitProp)) { // check all array values
-    var count = hitProp.length;
     var maxMatch = 0;
-    for (var i=0; i<count; i++) {
-      var match = propMatch(text, hitProp[i], numeric);
+    hitProp.forEach(function(value) {
+      var match = propMatch(text, value, numeric);
       if (match>maxMatch) {
         maxMatch=match;
       }
-    }
+    });
     return maxMatch;
   } else {
     return propMatch(text, hitProp, numeric);
@@ -321,8 +301,7 @@ function propMatchArray(text, hitProp, numeric) {
 
 
 /**
- * Check a defined parts of the parsed text address
- * against the results
+ * Check a defined part of the parsed text address
  *
  * @param {object} text
  * @param {object} hit
@@ -340,13 +319,15 @@ function checkAddressPart(text, hit, key) {
   }
   var score = propMatchArray(text[key], value, part.numeric);
 
-  if(key==='street' && hit.name) { // special case: proper version can be stored in the name
+  // special case: proper version can be stored in the name
+  // we need this because street name currently stores only one language
+  if(key==='street' && hit.name) {
     var _score = checkLanguageProperty(text[key], hit.name, true);
     if(_score>score) {
       score = _score;
     }
   }
-  logger.debug('address component match for ' + key, score);
+  logger.debug('address confidence for ' + key, score);
 
   return score;
 }
@@ -365,11 +346,12 @@ function checkAdmin(values, hit) {
     values = [values];
   }
 
-  // loop trough configured properties to find best match
   var sum=0, weightSum=0;
 
   values.forEach(function(value) {
     var best=0, weight = 1;
+
+    // loop trough configured properties to find best match
     for(var key in adminWeights) {
       var prop = hit.parent[key];
       if (prop) {
@@ -388,48 +370,8 @@ function checkAdmin(values, hit) {
     sum += weight*best;
     weightSum += weight;
   });
+
   return sum/weightSum;
 }
-
-
-/**
- * Check admin regions of the parsed text against a result.
- *
- * @param {object} text
- * @param {object} [text.regions]
- * @param {object} hit
- * @param {object} [hit.parent]
- * @returns {number}
- */
-function checkRegions(text, hit) {
-  var regions = [];
-  var source = text.regions;
-
-  for(var i=1; i<source.length; i++) { // drop 1st entry = actual name or street
-    regions.push(source[i]);
-  }
-
-  var bestMatch = checkAdmin(regions, hit);
-  logger.debug('admin match', bestMatch);
-
-  return bestMatch;
-}
-
-/**
- * Check city of the parsed text against a result.
- *
- * @param {string} city
- * @param {object} hit
- * @param {object} [hit.parent]
- * @returns {number}
- */
-function checkCity(city, hit) {
-
-  var bestMatch = checkAdmin(city, hit);
-  logger.debug('city match', bestMatch);
-
-  return bestMatch;
-}
-
 
 module.exports = setup;
