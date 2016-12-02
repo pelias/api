5bda15e74eab2c64f067fb184136d56459f89b1d
Peter Johnson <insomnia@rcpt.at>
2016-12-02 17:43:54 +0100

    update 500 middleware; add log statement, clean up code

--- middleware/500.js
+++ middleware/500.js
@@ -1,13 +1,19 @@
-var logger = require( 'pelias-logger' ).get( 'api' );
+var check = require('check-types'),
+    logger = require( 'pelias-logger' ).get( 'api' );
 
 // handle application errors
 function middleware(err, req, res, next) {
-  logger.error( 'Error: `%s`. Stack trace: `%s`.', err, err.stack );
-  res.header('Cache-Control','public');
-  var error = (err && err.message) ? err.message : err;
 
-  if( res.statusCode < 400 ){ res.status(500); }
-  res.json({ error: typeof error === 'string' ? error : 'internal server error' });
+  logger.error( 'Error: `%s`. Stack trace: `%s`.', err, err.stack );
+
+  if( res.statusCode < 400 ){
+    logger.info( 'status code changed from', res.statusCode, 'to 500' );
+    res.status(500);
+  }
+
+  var error = ( err && err.message ) ? err.message : err;
+  res.header('Cache-Control','public');
+  res.json({ error: check.nonEmptyString( error ) ? error : 'internal server error' });
 }
 
 module.exports = middleware;
