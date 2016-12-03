44ab6628b50ee7f020c6f3206d835801a949f50f
Dev <dev@pelias.fifthestate.com>
2016-12-02 19:25:05 -0500

    Added a clustered pm2 config

---
+++ pm2.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  /**
+   * Application configuration section
+   * http://pm2.keymetrics.io/docs/usage/application-declaration/
+   */
+  apps : [
+
+    // First application
+    {
+      name      : 'pelias',
+      instances : 'max',
+      exec_mode : 'cluster',
+      script    : 'index.js',
+      env_production : {
+        NODE_ENV: 'production'
+      }
+    },
+  ],
+
+  /**
+   * Deployment section
+   * http://pm2.keymetrics.io/docs/usage/deployment/
+   */
+};
