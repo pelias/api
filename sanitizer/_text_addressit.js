eed4be4c68739a62da1b29b7a296676b956d12fb
Vesa Meskanen <vesa.meskanen@logica.com>
2016-12-12 15:10:19 +0200

    Bugfix: libpostal sometimes maps city to postalcode. Fixed by validator regexp.
    Libpostal gets confused from searches such as 'Helsinki, Helsinki',
    'Espoo, Espoo' and 'Vantaa, Vantaa'. The city name is taken as postalcode.
    This problem is mitigated by an option to configure an arbitrary regexp string
    into peliasconfig.localization.postalCodeValidator field.

--- sanitizer/_text_addressit.js
+++ sanitizer/_text_addressit.js
@@ -13,10 +13,17 @@ var api = require('pelias-config').generate().api;
 // scores because WOF defines only international country names (Finland)
 var filteredRegions;
 var cleanRegions;
+var postalCodeValidator = function(code) { return true; }; // default = accept everything
 
 if (api && api.localization) {
   filteredRegions = api.localization.filteredRegions;
   cleanRegions = api.localization.cleanRegions;
+  if(api.localization.postalCodeValidator) {
+    postalCodeValidator = function(code) {
+      var regexp = new RegExp(api.localization.postalCodeValidator);
+      return regexp.test(code);
+    };
+  }
 }
 
 
@@ -78,13 +85,13 @@ function assignValidLibpostalParsing(parsedText, fromLibpostal, text) {
       }
     }
   }
-  // assume that numbers are always parsed correctly
-
+  // assume that sreet numbers are always parsed correctly
   if(check.assigned(fromLibpostal.number)) {
     parsedText.number = fromLibpostal.number;
   }
 
-  if(check.assigned(fromLibpostal.postalcode)) {
+  // validate postalcode
+  if(check.assigned(fromLibpostal.postalcode) && postalCodeValidator(fromLibpostal.postalcode)) {
     parsedText.postalcode = fromLibpostal.postalcode;
   }
 
