b5ec0ecd9e4af0bdabe1232609ef859c44d7fa08
Vesa Meskanen <vesa.meskanen@logica.com>
2016-12-29 13:54:09 +0200

    Option to normalize strings before confidence scoring
    Config file can contain a list of character transformations
    to be applied before property scoring. For example é and e
    can be mappped to match.

--- middleware/confidenceScoreDT.js
+++ middleware/confidenceScoreDT.js
@@ -9,7 +9,7 @@ var check = require('check-types');
 var _ = require('lodash');
 var fuzzy = require('../helper/fuzzyMatch');
 var languages = ['default'];
-
+var equalCharMap = {}, equalRegex = {};
 var adminWeights;
 var minConfidence=0, relativeMinConfidence;
 
@@ -39,9 +39,24 @@ function setup(peliasConfig) {
       if(localization.confidenceAddressParts) {
         confidenceAddressParts = localization.confidenceAddressParts;
       }
+      if(localization.equalCharMap) {
+        equalCharMap = localization.equalCharMap;
+        for(var c in equalCharMap) {
+          equalRegex[c] = new RegExp(c, 'gi');
+        }
+      }
     }
   }
   return computeScores;
+}
+
+
+// map chars which are considered equal in scoring
+function normalize(s) {
+  for(var c in equalCharMap) {
+    s = s.replace(equalRegex[c], equalCharMap[c]);
+  }
+  return s;
 }
 
 
@@ -208,17 +223,18 @@ function checkLanguageProperty(text, propertyObject, stripNumbers) {
   var bestScore = 0;
   var bestName;
 
+  text = normalize(text);
+
   for (var lang in propertyObject) {
     if (languages.indexOf(lang) === -1) {
       continue;
     }
     var score;
-
+    var text2 = normalize(propertyObject[lang]);
     if(stripNumbers) {
-      score = fuzzy.match(text, propertyObject[lang].replace(/[0-9]/g, '').trim());
-    } else {
-      score = fuzzy.match(text, propertyObject[lang]);
+      text2 = text2.replace(/[0-9]/g, '').trim();
     }
+    score = fuzzy.match(text, text2);
 
     if (score >= bestScore ) {
       bestScore = score;
@@ -280,7 +296,7 @@ function propMatch(textProp, hitProp, numeric) {
     }
   }
 
-  return fuzzy.match(textProp.toString(), hitProp.toString());
+  return fuzzy.match(normalize(textProp.toString()), normalize(hitProp.toString()));
 }
 
 // array wrapper for function above
@@ -357,9 +373,13 @@ function checkAdmin(values, hit) {
       if (prop) {
         var match;
         if ( Array.isArray(prop) ) {
-          match = fuzzy.matchArray(value, prop);
+          var nProp = [];
+          for(var i in prop) {
+            nProp.push(normalize(prop[i]));
+          }
+          match = fuzzy.matchArray(normalize(value), nProp);
         } else {
-          match = fuzzy.match(value, prop);
+          match = fuzzy.match(normalize(value), normalize(prop));
         }
         if(match>best) {
           best = match;
