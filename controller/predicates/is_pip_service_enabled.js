f57ac6fa0550f452618e84a2560d38249ee54421
Stephen Hess <stephen.k.hess@gmail.com>
2017-03-15 10:58:49 -0400

    removed lodash

--- controller/predicates/is_pip_service_enabled.js
+++ controller/predicates/is_pip_service_enabled.js
@@ -1,5 +1,3 @@
-const _ = require('lodash');
-
 module.exports = (uri) => {
   // this predicate relies upon the fact that the schema has already validated
   //  that api.pipService is a URI-formatted string
