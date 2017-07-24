b0d2e8c74a03d28d0792c136305ee6e1da5e39a5
Stephen Hess <stephen.k.hess@gmail.com>
2017-07-24 16:47:02 -0400

    capitalization

--- controller/predicates/is_addressit_parse.js
+++ controller/predicates/is_addressit_parse.js
@@ -1,4 +1,4 @@
 const _ = require('lodash');
 
-// returns true iff req.clean.parser is addressit
+// returns true IFF req.clean.parser is addressit
 module.exports = (req, res) => (_.get(req, 'clean.parser') === 'addressit');
