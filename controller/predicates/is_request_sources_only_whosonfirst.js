9ee50253120091806176e6fae362d0be3737472f
Lily He <yonglinlilyhe@gmail.com>
2017-08-07 18:19:02 -0400

    predicates debugging prints stack trace snippet

--- controller/predicates/is_request_sources_only_whosonfirst.js
+++ controller/predicates/is_request_sources_only_whosonfirst.js
@@ -1,9 +1,17 @@
 const _ = require('lodash');
+const Debug = require('../../helper/debug');
+const debugLog = new Debug('controller:predicates:is_request_sources_only_whosonfirst');
+const stackTraceLine = require('../../helper/stackTraceLine');
 
 // returns true IFF 'whosonfirst' is the only requested source
-module.exports = (req, res) => (
-  _.isEqual(
+module.exports = (req, res) => {
+  const is_request_sources_only_whosonfirst = _.isEqual(
     _.get(req, 'clean.sources', []),
     ['whosonfirst']
-  )
-);
+  );
+  debugLog.push(req, () => ({
+    reply: is_request_sources_only_whosonfirst,
+    stack_trace: stackTraceLine()
+  }));
+  return is_request_sources_only_whosonfirst;
+};
