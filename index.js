4d3ea56a397455ec819066ba1dd73c98c3a07503
Julian Simioni <julian@mapzen.com>
2017-10-23 13:00:03 -0400

    Change default listen parameter to undefined
    This will ensure our default listen address is whatever the Node.js
    default is. It's currently set to listen on all addresses, and probably
    won't change.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 const app = require('./app'),
     port = ( process.env.PORT || 3100 ),
-    host = ( process.env.HOST || 'localhost' );
+    host = ( process.env.HOST || undefined );
 
 /** run server on the default setup (single core) **/
 console.log( `pelias is now running on ${host}:${port}` );
