65507807acfe25da6b2db379cd72b01e3b304e62
Julian Simioni <julian@mapzen.com>
2017-10-23 13:03:10 -0400

    Ask Node's `net.server` for the actual address its listening on
    This allows us to emit a helpful and correct message when Pelias starts.

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const app = require('./app'),
     port = ( process.env.PORT || 3100 ),
     host = ( process.env.HOST || undefined );
 
-/** run server on the default setup (single core) **/
-console.log( `pelias is now running on ${host}:${port}` );
-app.listen( port, host );
+const server = app.listen( port, host, () => {
+  // ask server for the actual address and port its listening on
+  const listenAddress = server.address();
+  console.log( `pelias is now running on ${listenAddress.address}:${listenAddress.port}` );
+});
