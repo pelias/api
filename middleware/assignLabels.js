9676cdc2b8136de1c7ffd877f70ed0d04b1a54e5
erlendnils1 <erlendnils1@gmail.com>
2017-11-17 14:59:27 +0100

    DK-256 Add alias in () after proper name in labels, instead of municipality. Skip municipality for airport labels.

--- middleware/assignLabels.js
+++ middleware/assignLabels.js
@@ -17,14 +17,18 @@ function assignLabel(req, res, next, labelGenerator) {
 
   res.data.forEach(function (result) {
       // Entur: Use display name as default name when set. To display alias documents by their proper name
-      if (result && result.name && result.name.display) {
-          result.name.default = result.name.display;
-      }
-      result.label = labelGenerator(result);
-      // Entur - override default behaviour in pelias-labels deduping ', locality' part
-      // when locality = name. Instead adding county name in ( )
-      if (result.locality && result.county && result.name && result.name.default && !result.label.includes(',')) {
-        result.label = result.name.default + ' (' + result.county + ')';
+      if (result && result.name && result.name.alias && result.name.alias !== result.name.default) {
+          result.label = result.name.display +  ' (' + result.name.alias + ')';
+      } else if (result && result.name && result.name.default && result.category && result.category.includes('airport')) {
+          // Entur: Do not display municipality for airports
+          result.label=result.name.default;
+      }  else {
+          result.label = labelGenerator(result);
+          // Entur - override default behaviour in pelias-labels deduping ', locality' part
+          // when locality = name. Instead adding county name in ( )
+          if (result.locality && result.county && result.name && result.name.default && !result.label.includes(',')) {
+              result.label = result.name.default + ' (' + result.county + ')';
+          }
       }
   });
 
