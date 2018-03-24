8bb27bc84778bacb7cfd9be8fced6b93aa209a4e
Julian Simioni <julian@simioni.org>
2018-03-24 14:35:24 -0400

    Allow changing focus point from ENV var
    This is just for easy testing

--- query/autocomplete_defaults.js
+++ query/autocomplete_defaults.js
@@ -31,7 +31,7 @@ module.exports = _.merge({}, peliasQuery.defaults, {
   'focus:offset': '0km',
   'focus:scale': '250km',
   'focus:decay': 0.5,
-  'focus:weight': 15,
+  'focus:weight': process.env.AUTOCOMPLETE_FOCUS_WEIGHT || 15,
 
   'function_score:score_mode': 'avg',
   'function_score:boost_mode': 'replace',
