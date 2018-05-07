233b063e522fd2854cf970b38851822b2f94a866
missinglink <insomnia@rcpt.at>
2018-05-07 17:14:14 +0200

    feat(targets): configuration-based filter targets

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 
 const app = require('./app'),
+    typeMapping = require('./helper/type_mapping'),
     port = ( process.env.PORT || 3100 ),
     host = ( process.env.HOST || undefined );
 
-const server = app.listen( port, host, () => {
-  // ask server for the actual address and port its listening on
-  const listenAddress = server.address();
-  console.log( `pelias is now running on ${listenAddress.address}:${listenAddress.port}` );
+// ensure the dynamic type mappings are loaded before starting the server.
+typeMapping.load(() => {
+  const server = app.listen( port, host, () => {
+    // ask server for the actual address and port its listening on
+    const listenAddress = server.address();
+    console.log( `pelias is now running on ${listenAddress.address}:${listenAddress.port}` );
+  });
 });
