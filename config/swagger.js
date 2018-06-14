c9de4eb2e492a630ae3a0ceafd9508e10fefad36
Gibran Parvez <gibran.parvez@rgi-corp.com>
2018-06-14 11:20:54 -0400

    Add Authorization header auth

--- config/swagger.js
+++ config/swagger.js
@@ -4,7 +4,18 @@ const options = {
         'description': 'Swagger documentation for Pelias API',
         'title': 'Pelias API',
         'version': '1.0.0'
-    }
+    },
+    'schemes': ['http','https'],
+    'securityDefinitions': { 
+        'Bearer': {
+            'type': 'apiKey',
+            'name': 'Authorization',
+            'in': 'header'
+        }
+    },
+    'security': [
+       { 'Bearer': []} 
+    ]
     },
     'basePath': __dirname,
     'apis': ['./routes/*.js']
