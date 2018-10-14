6d9e51119af2cf3261e1c5f76cafc6ca6af3f4cb
Julian Simioni <julian@simioni.org>
2018-10-14 10:18:02 -0400

    Use test description consistent with fixture file

--- test/unit/query/search_original.js
+++ test/unit/query/search_original.js
@@ -35,7 +35,7 @@ module.exports.tests.query = function(test, common) {
     var expected = require('../fixture/search_linguistic_focus_bbox_original');
 
     t.deepEqual(compiled.type, 'search_original', 'query type set');
-    t.deepEqual(compiled.body, expected, 'search_linguistic_focus_bbox');
+    t.deepEqual(compiled.body, expected, 'search_linguistic_focus_bbox_original');
     t.end();
   });
 
