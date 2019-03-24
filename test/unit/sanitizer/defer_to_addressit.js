2b2a6a4c73a53bfa5d79cf3912bd1c983c4485be
Julian Simioni <julian@simioni.org>
2019-03-24 18:00:52 -0400

    fix(logging): Remove log of every query sent to addressit
    This is pretty verbose as its fairly common, and we can determine what
    queries were sent to addressit in other ways.

--- test/unit/sanitizer/defer_to_addressit.js
+++ test/unit/sanitizer/defer_to_addressit.js
@@ -37,7 +37,7 @@ module.exports.tests.sanitize = (test, common) => {
   });
 
   test('verify that _text_addressit sanitizer was called when should_execute returns true', (t) => {
-    t.plan(3);
+    t.plan(2);
 
     const logger = mock_logger();
 
@@ -74,7 +74,6 @@ module.exports.tests.sanitize = (test, common) => {
     };
 
     defer_to_addressit(req, {}, () => {
-      t.deepEquals(logger.getInfoMessages(), ['fallback queryText: this is the query text']);
       t.end();
     });
 
@@ -120,51 +119,6 @@ module.exports.tests.sanitize = (test, common) => {
     });
 
   });
-
-  test('query should be logged as [text removed] if private', (t) => {
-    t.plan(3);
-
-    const logger = mock_logger();
-
-    // rather than re-verify the functionality of all the sanitizers, this test just verifies that they
-    //  were all called correctly
-    const defer_to_addressit = proxyquire('../../../sanitizer/defer_to_addressit', {
-      '../sanitizer/_text_addressit': function () {
-        return {
-          sanitize: () => {
-            t.pass('_text_addressit should have been called');
-            return { errors: [], warnings: [] };
-          }
-        };
-      },
-      'pelias-logger': logger,
-      '../helper/logging': {
-        isDNT: () => true
-      },
-      '../sanitizer/_debug': () => {
-        return {
-          sanitize: () => {
-            t.pass('_debug should have been called');
-          return { errors: [], warnings: [] };
-          }
-        };
-      }
-    })(() => true);
-
-    const req = {
-      path: '/v1/search',
-      clean: {
-        text: 'this is the query text'
-      }
-    };
-
-    defer_to_addressit(req, {}, () => {
-      t.deepEquals(logger.getInfoMessages(), ['fallback queryText: [text removed]']);
-      t.end();
-    });
-
-  });
-
 };
 
 module.exports.all = function (tape, common) {
