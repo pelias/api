9dc675e802c53ed92150a5636d0b18dd7cd7bb78
missinglink <insomnia@rcpt.at>
2019-10-09 15:16:42 +0200

    feat(dedupe): improved deduplication of places containing their placetype in the name
    Connects https://github.com/pelias/geonames/issues/395

--- helper/diffPlaces.js
+++ helper/diffPlaces.js
@@ -119,6 +119,11 @@ function isNameDifferent(item1, item2, requestLanguage){
   // note: this really shouldn't happen as name is a mandatory field
   if( !isPojo1 || !isPojo2 ){ return false; }
 
+  // apply 'layer dependent normalization' to the names
+  // this ensures that 'Foo' and 'City of Foo' match for localities.
+  names1 = layerDependentNormalization(names1, _.get(item1, 'layer'));
+  names2 = layerDependentNormalization(names2, _.get(item2, 'layer'));
+
   // else both have name info
 
   // iterate over all the languages in item2, comparing them to the
@@ -273,6 +278,61 @@ function getPlaceTypeRank(item) {
  * apply unicode normalization, lowercase characters and remove
  * diacritics and some punctuation.
  */
+function layerDependentNormalization(names, layer) {
+
+  // sanity checking inputs
+  if (!_.isPlainObject(names)) { return names; }
+  if (!_.isString(layer)) { return names; }
+
+  // clone the names to avoid mutating the response data
+  const copy = _.cloneDeep(names);
+
+  // region
+  if (layer === 'region') {
+    _.forEach(names, (value, lang) => {
+      copy[lang] = field.getArrayValue(value).map(name => {
+        return name
+          .replace(/^state\sof(?!\s?the)\s?(.*)$/i, '$1')
+          .replace(/^(.*)\sstate$/i, '$1')
+          .trim();
+      });
+    });
+  }
+
+  // county
+  if( layer === 'county' ){
+    _.forEach(names, (value, lang) => {
+      copy[lang] = field.getArrayValue(value).map(name => {
+        return name
+          .replace(/^county\sof(?!\s?the)\s?(.*)$/i, '$1')
+          .replace(/^(.*)\scounty$/i, '$1')
+          .trim();
+      });
+    });
+  }
+
+  // locality/localadmin
+  if (layer === 'locality' || layer === 'localadmin') {
+    _.forEach(names, (value, lang) => {
+      copy[lang] = field.getArrayValue(value).map(name => {
+        return name
+          .replace(/^city\sof(?!\s?the)\s?(.*)$/i, '$1')
+          .replace(/^(.*)\scity$/i, '$1')
+          .replace(/^town\sof(?!\s?the)\s?(.*)$/i, '$1')
+          .replace(/^(.*)\stown$/i, '$1')
+          .replace(/^township\sof(?!\s?the)\s?(.*)$/i, '$1')
+          .replace(/^(.*)\stownship$/i, '$1')
+          .trim();
+      });
+    });
+  }
+
+  return copy;
+}
+
+/**
+ * lowercase characters and remove diacritics and some punctuation
+ */
 function normalizeString(str){
   return removeAccents(unicode.normalize(str)).toLowerCase().split(/[ ,-]+/).join(' ');
 }
@@ -281,3 +341,4 @@ module.exports.isDifferent = isDifferent;
 module.exports.layerPreferences = layerPreferences;
 module.exports.isNameDifferent = isNameDifferent;
 module.exports.normalizeString = normalizeString;
+module.exports.layerDependentNormalization = layerDependentNormalization;
