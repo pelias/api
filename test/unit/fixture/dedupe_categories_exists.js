a7403d2d8990c4724471f0b659afa6c3e74fbdbf
Julian Simioni <julian@simioni.org>
2020-04-28 08:48:04 -0700

    WIP

---
+++ test/unit/fixture/dedupe_categories_exists.js
@@ -0,0 +1,25 @@
+module.exports = [
+  {
+    '_id': '101914069',
+    'layer': 'venue',
+    'source': 'openstreetmap',
+    'name': {
+      'default': 'A place'
+    },
+    'parent': {
+      'country_a': ['USA']
+    }
+  },
+  {
+    '_id': '323',
+    'layer': 'venue',
+    'source': 'openstreetmap',
+    'name': {
+      'default': 'A place'
+    },
+    'categories': [ 'category1', 'category2' ],
+    'parent': {
+      'country_a': ['USA']
+    }
+  }
+];
