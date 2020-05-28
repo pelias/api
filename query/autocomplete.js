1b69c9904d42dc498d33d3f28fd3001b9013c3d6
Julian Simioni <julian@simioni.org>
2020-05-28 11:10:20 -0700

    feat(temporary): Only query by language if additional param set
    This is just for testing and rollout, it's not intended to be permanent
    :)

--- query/autocomplete.js
+++ query/autocomplete.js
@@ -183,7 +183,7 @@ function generateQuery( clean ){
   if ( isAdminSet ){ vs.var('input:add_name_to_multimatch', 'enabled'); }
 
   // Search in the user lang
-  if(clean.lang && _.isString(clean.lang.iso6391)) {
+  if(clean.lang && clean.lang.queryByLang && _.isString(clean.lang.iso6391)) {
     vs.var('lang', clean.lang.iso6391);
   }
 
