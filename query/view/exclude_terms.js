b869c29efc9b84a9607c87382187585b10cb5c06
Tom Erik Støwer <testower@gmail.com>
2021-04-29 20:27:20 +0200

    WTK-424 Add exclude_terms view. Will require query post-processing

---
+++ query/view/exclude_terms.js
@@ -0,0 +1,19 @@
+/**
+ * Entur:
+ * This view adds a must_not predicate to the query.
+ * Note that pelias-query does not support rendering must_not predicates
+ * so post-processing must be applied to the rendered query 
+ */
+module.exports = function ( property, value ) {
+    return function( vs ) {
+        var query = {
+            must_not: {
+                terms: {
+                    [property]: value
+                }
+            }
+        };
+        
+        return query;
+    };
+};
