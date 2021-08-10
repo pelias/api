2a0c64e8a9daaf1373d4cf658882c1fc149fe6bf
Tom Erik Støwer <testower@gmail.com>
2021-08-10 19:19:18 +0200

    Revert "Use graceful shutdown library that should monitor and close open connections during shutdown"
    This reverts commit 704df4962bb834520ed0a089c6b7a6ed1faf1718.

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const logger = require('pelias-logger').get('api');
-const GracefulShutdownManager = require('@moebius/http-graceful-shutdown').GracefulShutdownManager;
 
 const app = require('./app'),
     port = ( process.env.PORT || 3100 ),
@@ -11,13 +10,16 @@ const server = app.listen( port, host, () => {
   console.log( `pelias is now running on ${listenAddress.address}:${listenAddress.port}` );
 });
 
-const shutdownManager = new GracefulShutdownManager(server);
-
 function exitHandler(signal) {
   logger.info(`Received ${signal}. Starting graceful shutdown.`);
 
-  shutdownManager.terminate(() => {
+  server.close((err) => {
+    if (err) {
+      logger.error(err);
+      process.exit(1);
+    }
     logger.info('Graceful shutdown complete. Exiting.');
+    process.exit(0);
   });
 }
 
