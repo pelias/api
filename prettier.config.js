c003b4f9341fd9086c56e1180e5d3119c9fc827e
Vinayak Kulkarni <19776877+vinayakkulkarni@users.noreply.github.com>
2022-01-04 17:39:46 +0530

    feat: add eslintrc, prettier config files

---
+++ prettier.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  $schema: 'http://json.schemastore.org/prettierrc',
+  semi: true,
+  arrowParens: 'always',
+  singleQuote: true,
+  trailingComma: 'all',
+  bracketSpacing: true,
+  htmlWhitespaceSensitivity: 'css',
+  insertPragma: false,
+  tabWidth: 2,
+  useTabs: false,
+  endOfLine: 'lf',
+};
