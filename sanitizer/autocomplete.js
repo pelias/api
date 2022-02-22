76f5c0435e9a856d9c4a227295fcf7a862b37387
Julian Simioni <julian@simioni.org>
2022-02-22 13:44:01 -0500

    Reorder sanitizers so address layer filter can look at boundary params

--- sanitizer/autocomplete.js
+++ sanitizer/autocomplete.js
@@ -10,7 +10,6 @@ module.exports.middleware = (_api_pelias_config) => {
       size: require('../sanitizer/_size')(/* use defaults*/),
       layers: require('../sanitizer/_targets')('layers', type_mapping.layer_mapping),
       sources: require('../sanitizer/_targets')('sources', type_mapping.source_mapping),
-      address_layer_filter: require('../sanitizer/_address_layer_filter')(type_mapping),
       tokenizer: require('../sanitizer/_tokenizer')(),
       // depends on the layers and sources sanitizers, must be run after them
       sources_and_layers: require('../sanitizer/_sources_and_layers')(),
@@ -18,6 +17,7 @@ module.exports.middleware = (_api_pelias_config) => {
       location_bias: require('../sanitizer/_location_bias')(_api_pelias_config.defaultParameters),
       geo_autocomplete: require('../sanitizer/_geo_autocomplete')(),
       boundary_country: require('../sanitizer/_boundary_country')(),
+      address_layer_filter: require('../sanitizer/_address_layer_filter')(type_mapping),
       categories: require('../sanitizer/_categories')(),
       request_language: require('../sanitizer/_request_language')(),
       boundary_gid: require('../sanitizer/_boundary_gid')()
