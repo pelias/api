966ee9e6a5d907467b4c7700d0fe6cca66f436cd
Julian Simioni <julian@simioni.org>
2022-05-10 11:58:02 -0400

    Load type mapping before starting webserver

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const logger = require('pelias-logger').get('api');
+const type_mapping = require('./helper/type_mapping');
+
 const app = require('./app'),
     port = ( process.env.PORT || 3100 ),
     host = ( process.env.HOST || undefined );
 
-const server = app.listen( port, host, () => {
-  // ask server for the actual address and port its listening on
-  const listenAddress = server.address();
-  logger.info( `pelias is now running on http://${listenAddress.address}:${listenAddress.port}` );
+let server;
+
+// load Elasticsearch type mappings before starting web server
+type_mapping.load(() => {
+  server = app.listen( port, host, () => {
+    // ask server for the actual address and port its listening on
+    const listenAddress = server.address();
+    logger.info( `pelias is now running on http://${listenAddress.address}:${listenAddress.port}` );
+  });
 });
 
 function exitHandler() {
