096a41a7f039a9521ca06d8a85ce78031783a4d9
Pavlo Rosiuk <pavlo.rosiuk@entur.org>
2024-01-30 10:16:24 +0100

    update harness var && remove terraform

--- terraform/backend.tf
+++
@@ -1,8 +0,0 @@
-
-  # Describe where terraform will store the state of infrastructure
-  terraform {
-    backend "gcs" {
-    bucket = "entur-system-tf-backend-ror"
-    prefix = "gcp/ror/pelias"
-    }
-  }
--- terraform/env/kub-ent-tst.tfvars
+++
@@ -1,3 +0,0 @@
-gcp_project            = "entur-test-1287"
-kube_namespace = "staging"
-storage_bucket_name="kakka-test"
