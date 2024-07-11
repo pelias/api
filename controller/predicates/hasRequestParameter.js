e0326c7591d5daf1a30ca58f298aa4a909a0ba45
Peter Johnson <insomnia@rcpt.at>
2024-07-11 17:35:22 +0200

    fix(predicate): request params with a key but no value should be considered unset

--- controller/predicates/hasRequestParameter.js
+++ controller/predicates/hasRequestParameter.js
@@ -3,13 +3,15 @@ const Debug = require('../../helper/debug');
 const debugLog = new Debug('controller:predicates:has_request_parameter');
 const stackTraceLine = require('../../helper/stackTraceLine');
 
-// returns true IFF req.clean has a key with the supplied name
+// returns true IFF req.clean has a key with the supplied name AND a non-empty value
 module.exports = (parameter) => (req, res) => {
-  const has_request_parameter = _.has(req, ['clean', parameter]);
+  const value = _.get(req, ['clean', parameter]);
+  const has_request_parameter = _.isNumber(value) || !_.isEmpty(value);
 
   debugLog.push(req, () => ({
     reply: {[parameter]: has_request_parameter},
     stack_trace: stackTraceLine()
   }));
+
   return has_request_parameter;
 };
