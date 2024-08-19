cafe440237c252023c70510891c9ddb94511cd5d
Tom Erik Støwer <tom.erik.stower@entur.org>
2024-08-19 10:34:23 +0200

    Shutdown improvements

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,9 @@
 FROM pelias/libpostal_baseimage
 
+ENV TINI_VERSION v0.19.0
+ADD https://github.com/krallin/tini/releases/download/${TINI_VERSION}/tini /tini
+RUN chmod +x /tini
+
 RUN mkdir -p /var/log/esclient/
 RUN chown pelias /var/log/esclient
 
@@ -23,4 +27,4 @@ ENV PELIAS_CONFIG=${WORK}/pelias.json
 ENV PORT 3000
 EXPOSE 3000
 
-CMD ["node","/home/pelias/index.js" ]
+ENTRYPOINT ["/tini", "--", "node","/home/pelias/index.js"]
--- index.js
+++ index.js
@@ -7,27 +7,30 @@ const port = ( process.env.PORT || 3100 );
 const host = ( process.env.HOST || undefined );
 
 function onSignal () {
-  logger.info('server is starting cleanup');
+  logger.warn('server is starting cleanup');
   return Promise.all([
     // your clean logic, like closing database connections
   ]);
 }
 
 function beforeShutdown () {
-  // given your readiness probes run every 5 second
-  // may be worth using a bigger number so you won't
-  // run into any race conditions
+  logger.warn('server will shut down');
+
   return new Promise(resolve => {
-    setTimeout(resolve, 10000);
+    setTimeout(resolve, 20000);
   });
 }
 
 function onShutdown () {
-  logger.info('cleanup finished, server is shutting down');
+  logger.warn('cleanup finished, server is shutting down');
 }
 
 function healthCheck ({ state }) {
   // `state.isShuttingDown` (boolean) shows whether the server is shutting down or not
+  if (state.isShuttingDown) {
+    return Promise.reject();
+  }
+
   return Promise.resolve(
     // optionally include a resolve value to be included as
     // info in the health check response
@@ -47,7 +50,7 @@ const options = {
   onSignal,
   beforeShutdown,
   onShutdown,
-
+  useExit0: true,
   logger: logger.error
 };
 
@@ -56,6 +59,5 @@ createTerminus(server, options);
 server.listen( port, host, () => {
   // ask server for the actual address and port its listening on
   const listenAddress = server.address();
-  logger.info( `pelias is now running on ${listenAddress.address}:${listenAddress.port}` );
+  logger.warn( `pelias is now running on ${listenAddress.address}:${listenAddress.port}` );
 });
-
