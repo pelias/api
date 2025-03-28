2a88d37b0c42b8bb443b2e3e21ef4962d04011aa
Julian Simioni <julian@simioni.org>
2025-03-28 11:28:42 -0400

    Skip language service lookup for parent IDs same as the record
    WOF and Geonames andmin records usually have a parent record in the same
    layer as their own which lists their own ID.
    
    For example the Geonames record for NYC has its own ID in the
    locality_id field.
    
    If present, any translations for the record itself will already be on
    the record, so there is no sense in looking them up.
    
    It especially makes no sense for Geonames records, since the language
    service does not have any translations for Geonames at all.

--- service/configurations/Language.js
+++ service/configurations/Language.js
@@ -10,11 +10,26 @@ class Language extends ServiceConfiguration {
   }
 
   getParameters(req, res) {
-    // find all the values for all keys with names that end with '_id'
-    const ids = _.get(res, 'data', []).reduce((acc, doc) => {
-      Array.prototype.push.apply(acc, _.values(_.pickBy(doc.parent, (v, k) => _.endsWith(k, '_id') ) ) );
-      return acc;
-    }, []);
+    // find all the values for all parent _id keys and are not the same ID as the record itself
+    const ids = _.get(res, 'data', []).map((doc) => {
+      const fields = _.pickBy(doc.parent, (value, key) => {
+        if (!_.endsWith(key, '_id')) {
+          return false;
+        }
+
+        // Don't attempt to translate admin records that actually point to this record
+        if (value === doc.source_id) {
+          return false;
+        }
+
+        return true;
+      });
+      return _.values(fields);
+    });
+    //const ids = _.get(res, 'data', []).reduce((acc, doc) => {
+      //Array.prototype.push.apply(acc, _.values(_.pickBy(doc.parent, (v, k) => _.endsWith(k, '_id') ) ) );
+      //return acc;
+    //}, []);
     const lang = _.get(req, 'clean.lang.iso6393');
     const parameters = {
       // arrays will be nested, so flatten first, then uniqify, and finally join elements with comma
